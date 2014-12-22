b94ef41ef2cdd6c7bf7b6d39246893406aeda4f7
Joe Thorley <joe@poissonconsulting.ca>
2014-12-21 17:56:19 -0800

    renamed Function as Average and rearranged columns

--- data-raw/use-guidelines-data.R
+++ data-raw/use-guidelines-data.R
@@ -5,9 +5,9 @@ use_guidelines <- function () {
 
   stopifnot(identical(colnames(guidelines),
                       c("Parameter", "Form", "Jurisdiction",
-                        "Use", "Samples", "Days", "Function",
+                        "Use", "Samples", "Days", "Average",
                         "Condition", "Guideline", "Unit",
-                        "Date", "URL", "Comments")))
+                        "Comments", "Date", "URL")))
 
   stopifnot(identical(levels(guidelines$Form),
                       c("Dissolved", "E.coli", "Enterococci", "Total")))
@@ -20,17 +20,30 @@ use_guidelines <- function () {
                         "Marine Life", "Recreation", "Wildlife")))
 
   stopifnot(identical(levels(guidelines$Unit),
-                      c("/100mL", "mg/L", "ug/L")))
+                      c("/100mL", "C", "m", "mg/L", "NTU", "ug/L")))
 
   stopifnot(is.integer(guidelines$Samples))
   stopifnot(is.integer(guidelines$Days))
 
+  stopifnot(all(!is.na(guidelines$Parameter)))
+  stopifnot(all(!is.na(guidelines$Jurisdiction)))
+  stopifnot(all(!is.na(guidelines$Use)))
+  stopifnot(all(!is.na(guidelines$Samples)))
+  stopifnot(all(!is.na(guidelines$Days)))
+  stopifnot(all(!is.na(guidelines$Average)))
+#  stopifnot(all(!is.na(guidelines$Guideline)))
+#  stopifnot(all(!is.na(guidelines$Unit)))
+  stopifnot(all(!is.na(guidelines$Date)))
+#  stopifnot(all(!is.na(guidelines$Url)))
+
   guidelines$Comments <- as.character(guidelines$Comments)
   guidelines$Date <- as.Date(as.character(guidelines$Date))
+
+# check url if internet connection
 
   devtools::use_data(guidelines, overwrite = TRUE, compress = "xz")
 
   invisible(guidelines)
 }
 
-use_guidelines()
+summary(use_guidelines())
