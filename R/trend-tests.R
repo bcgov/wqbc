36121461b1d0a7367e856099bb10a7d0725fd5ec
colinpmillar <colinpmillar@googlemail.com>
2014-12-27 08:53:35 +0000

    added trend-tests.R with skeleton help documentation

---
+++ R/trend-tests.R
@@ -0,0 +1,31 @@
+#' Test for Trends in Water Quality Indices
+#'
+#' Tests for linear and non-linear trends in water quality indices
+#' 
+#' [DRAFT]
+#'
+#' If the argument \code{x} must have columns: 
+#' WQI (0 to 100), 
+#' Year (or whichever single column is specified by the argument Year). 
+#' 
+#' only one WQI value for each year is allowed. 
+#' If a by argument is specified for example if by = c("Lake", "Site") 
+#' then a trend test is conducted for each combination of Site
+#' within each Lake by Year 
+#' 
+#' The function returns an object of class wq_trend which inherits from data.frame. It
+#' the will include any columns listed in the \code{by} argument plus any columns which do
+#' not vary for each Trend test (allows things like Lat and Lon to be passed
+#' through for plotting particular sites) plus the columns From and To which give
+#' the first and last Year for each trend test plus the column Values which gives
+#' the actual number of WQI values in the test plus the columns Trend (which
+#' gives the positive or negative net rate of change per scale unit) and
+#' Significance (p-value).
+#'
+#' @param x data.frame with columns WQI and time defined in \code{scale}
+#' @param scale the time stamp of the observation which must be a named column of \code{x}
+#' @param by a factor or a list of factors, which must be named columns of  \code{x}.
+#' @export
+test_trends <- function (x, scale = "Year", by = NULL) {
+  NA
+}
