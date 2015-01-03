efb6bbe6141971223688d8d7b9ae08f33625e603
joethorley <joe@poissonconsulting.ca>
2015-01-03 11:45:12 -0800

    updated code to manipulate variables and guidelines raw data and drop missing value rows

--- data-raw/use-guidelines-data.R
+++ data-raw/use-guidelines-data.R
@@ -1,15 +1,10 @@
 input_guidelines <- function () {
   require(devtools)
 
-  variables <- read.csv("data-raw/variables.csv", na.strings = c("NA", ""), stringsAsFactors = FALSE)
   guidelines <- read.csv("data-raw/guidelines.csv", na.strings = c("NA", ""), stringsAsFactors = FALSE)
 
-  n <- nrow(guidelines)
-  guidelines <- merge(variables, guidelines, by = "Variable")
-  stopifnot(n == nrow(guidelines))
-
   stopifnot(identical(colnames(guidelines),
-                      c("Variable", "Code", "Jurisdiction",
+                      c("Variable", "Jurisdiction",
                         "Use", "SubUse", "Samples", "Days", "Average",
                         "Condition", "Guideline", "Unit",
                         "Comments", "Date", "URL", "TableNumber")))
@@ -24,7 +19,11 @@ input_guidelines <- function () {
   stopifnot(identical(sort(unique(guidelines$Unit)),
                       c("/dL", "m", "mg/L", "NTU", "pH", "ug/L")))
 
-  guidelines$Date <- gsub("--", "-", guidelines$Date)
+  #  guidelines$Unit <- gsub("µg/L", "ug/L", guidelines$Unit)
+  #  guidelines$Unit <- gsub(" ", "", guidelines$Unit)
+  #  guidelines$Date <- gsub("--", "-", guidelines$Date)
+  #  write.csv(guidelines, "data-raw/guidelines.csv", row.names = FALSE)
+
   guidelines$Date <- as.Date(guidelines$Date)
 
   stopifnot(is.integer(guidelines$Samples))
@@ -41,12 +40,36 @@ input_guidelines <- function () {
   stopifnot(all(!is.na(guidelines$Date)))
   stopifnot(all(!is.na(guidelines$URL)))
 
+  variables <- read.csv("data-raw/variables.csv", na.strings = c("NA", ""), stringsAsFactors = FALSE)
+
+  stopifnot(identical(colnames(variables), c("Code", "Variable")))
+
+  n <- nrow(guidelines)
+  guidelines <- merge(variables, guidelines, by = "Variable")
+  stopifnot(n == nrow(guidelines))
+
   guidelines$Variable <- factor(guidelines$Variable)
   guidelines$Code <- factor(guidelines$Code)
+
+  stopifnot(all(!is.na(guidelines$Code)))
+
   guidelines$Jurisdiction <- factor(guidelines$Jurisdiction)
+
   guidelines$Use <- factor(guidelines$Use, levels = c(
     "Freshwater Life", "Marine Life", "Drinking", "Livestock",
     "Wildlife", "Irrigation", "Recreation"))
+
+  guidelines <- guidelines[!is.na(guidelines$Variable) &
+                             !is.na(guidelines$Code) &
+                             !is.na(guidelines$Jurisdiction) &
+                             !is.na(guidelines$Use) &
+                             !is.na(guidelines$Sample) &
+                             !is.na(guidelines$Days) &
+                             !is.na(guidelines$Average) &
+                             !is.na(guidelines$Guideline) &
+                             !is.na(guidelines$Unit) &
+                             !is.na(guidelines$Date) &
+                             !is.na(guidelines$URL),,drop = FALSE]
 
   guidelines
 }
@@ -54,4 +77,3 @@ rm(guidelines)
 guidelines <- input_guidelines()
 summary(guidelines)
 devtools::use_data(guidelines, overwrite = TRUE, compress = "xz")
-
