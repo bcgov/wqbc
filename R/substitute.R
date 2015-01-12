666166e188349cf78d7f6a04dcc0accbb64b975a
joethorley <joe@poissonconsulting.ca>
2015-01-12 14:14:30 -0800

    added substitute_names for processing data.frames

--- R/substitute.R
+++ R/substitute.R
@@ -43,7 +43,7 @@ substitute_units <-function (x, messages = TRUE) {
 #' Substitute Column Names
 #'
 #' Where possible substitute column names
-#' with recognised names
+#' with recognised column names
 #'
 #' @param x character vector of column names to substitute
 #' @param messages flag indicating whether to print messages
@@ -67,4 +67,50 @@ substitute_colnames <-function (x, messages = TRUE) {
   sub <- c("Date", "Variable", "Value", "Units", "Lat", "Long")
 
   substitute(x, sub, messages)
+}
+
+#' Substitute Variable Names
+#'
+#' Where possible substitute variable names
+#' with recognised variable names
+#'
+#' @param x character vector of variable names to substitute
+#' @param messages flag indicating whether to print messages
+#' @return character vector of substituted or original names
+#' @examples
+#' substitute_variables(c("aluminum", "totalNitrogen"))
+#' @export
+substitute_variables <-function (x, messages = TRUE) {
+  assert_that(is.character(x) || is.factor(x))
+  assert_that(is.flag(messages) && noNA(messages))
+
+  x <- as.character(x)
+  x <- data.frame(x = tolower(x), original = x, stringsAsFactors = FALSE)
+  x$x <- gsub(" ", "", x$x)
+
+  substitute(x, sub = get_variables(), messages)
+}
+
+#' Substitute Names
+#'
+#' Where possible substitute  names
+#' with recognised names
+#'
+#' @param x data.frame
+#' @param messages flag indicating whether to print messages
+#' @return data.frame
+#' @examples
+#'
+#' x <- data.frame(variable = c("total phosphorus", "arsenic"),
+#'                value = c(1, 2),
+#'                units = c("MG / L", "Ug/l "))
+#' substitute_names(x)
+#'
+#' @export
+substitute_names <-function (x, messages = TRUE) {
+  assert_that(is.data.frame(x))
+  colnames(x) <- substitute_colnames(colnames(x), messages = messages)
+  x$Units <- substitute_units(x$Units, messages = messages)
+  x$Variable <- substitute_variables(x$Variable, messages = messages)
+  x
 }
