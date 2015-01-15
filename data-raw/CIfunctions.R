4413b7d639d7778e0d4bb9679910870dd532a785
Andy Teucher <andy.teucher@gmail.com>
2015-01-15 11:43:31 -0800

    tidy up code formatting using formatR::tidy_source

--- data-raw/CIfunctions.R
+++ data-raw/CIfunctions.R
@@ -4,53 +4,56 @@
 
 #
 
-library(boot) # needed for row-wise bootstrapping
+library(boot)  # needed for row-wise bootstrapping
 
 
 
-AllInd<-function(DtObj,i=1:nrow(DtObj)){
+AllInd <- function(DtObj, i = 1:nrow(DtObj)) {
 
-# We find F1, F2, F3 and WQI. Here DtObj is the ratio of Data and Objective
+  # We find F1, F2, F3 and WQI. Here DtObj is the ratio of Data and
+  # Objective
 
-# matrix. We do so in order to avoid duplicated computation in bootstrapping.
+  # matrix. We do so in order to avoid duplicated computation in
+  # bootstrapping.
 
-# This function can be used for column as well as row bootstrapping.
+  # This function can be used for column as well as row bootstrapping.
 
-#
+  #
 
-# DtObj  : Ratio of Data and Objective matrix
+  # DtObj : Ratio of Data and Objective matrix
 
-# i      : Defined for row-bootstrapping.
+  # i : Defined for row-bootstrapping.
 
-#
+  #
 
-  DtObj<-DtObj[i,]
+  DtObj <- DtObj[i, ]
 
-  N<-nrow(DtObj);P<-ncol(DtObj)
+  N <- nrow(DtObj)
+  P <- ncol(DtObj)
 
-  NP1<-sum(!is.na(DtObj))
+  NP1 <- sum(!is.na(DtObj))
 
-  TestAllNA<-apply(DtObj,2,function(x) sum(is.na(x))!=N)
+  TestAllNA <- apply(DtObj, 2, function(x) sum(is.na(x)) != N)
 
-  ActP<-sum(TestAllNA) # Actual P, remove variables which have all
+  ActP <- sum(TestAllNA)  # Actual P, remove variables which have all
 
-                       # entries as NA
+  # entries as NA
 
-  FailCases<-DtObj>1
+  FailCases <- DtObj > 1
 
-  FailedVar<-apply(FailCases,2,any,na.rm=TRUE)
+  FailedVar <- apply(FailCases, 2, any, na.rm = TRUE)
 
-  F1<-sum(FailedVar, na.rm=TRUE)/ActP*100
+  F1 <- sum(FailedVar, na.rm = TRUE)/ActP * 100
 
-  F2<-sum(FailCases, na.rm=TRUE)/NP1*100
+  F2 <- sum(FailCases, na.rm = TRUE)/NP1 * 100
 
-  nse<-(sum(DtObj[DtObj>1],na.rm=TRUE)-sum(DtObj>1,na.rm=TRUE))/NP1
+  nse <- (sum(DtObj[DtObj > 1], na.rm = TRUE) - sum(DtObj > 1, na.rm = TRUE))/NP1
 
-  F3<-nse/(nse+1)*100
+  F3 <- nse/(nse + 1) * 100
 
-  WQI<-100-sqrt(F1^2+F2^2+F3^2)/1.732
+  WQI <- 100 - sqrt(F1^2 + F2^2 + F3^2)/1.732
 
-  c(F1=F1,F2=F2,F3=F3,WQI=WQI)
+  c(F1 = F1, F2 = F2, F3 = F3, WQI = WQI)
 
 }
 
@@ -60,63 +63,67 @@ AllInd<-function(DtObj,i=1:nrow(DtObj)){
 
 
 
-BootIndex<-function(DataByObj, R=10000){
+BootIndex <- function(DataByObj, R = 10000) {
 
-# This function finds the estimates in column-wise bootstraping procedure.
+  # This function finds the estimates in column-wise bootstraping
+  # procedure.
 
-#
+  #
 
-# DataByObj : Data divided by objective (accordingly)
+  # DataByObj : Data divided by objective (accordingly)
 
-# R         : Number of bootstrap simulations
+  # R : Number of bootstrap simulations
 
-#
+  #
 
-  N<-nrow(DataByObj)
+  N <- nrow(DataByObj)
 
-  P<-ncol(DataByObj)
+  P <- ncol(DataByObj)
 
-  FailCases<-DataByObj>1
+  FailCases <- DataByObj > 1
 
-  FailedVar<-apply(FailCases,2,any,na.rm=TRUE)
+  FailedVar <- apply(FailCases, 2, any, na.rm = TRUE)
 
-  ind<-1:length(FailedVar)
+  ind <- 1:length(FailedVar)
 
-  FailCol<- ind[FailedVar]  # these are the columns with failed variable
+  FailCol <- ind[FailedVar]  # these are the columns with failed variable
 
-  NFC<-length(FailCol)
-
-
-
-  NotFail<-ind[!FailedVar]
+  NFC <- length(FailCol)
 
 
 
-  BootData<-matrix(NA,ncol=P, nrow=N)
+  NotFail <- ind[!FailedVar]
 
-  for (i in 1:length(NotFail)){
 
-     BootData[,NotFail[i]]<-DataByObj[,NotFail[i]]
+
+  BootData <- matrix(NA, ncol = P, nrow = N)
+
+  for (i in 1:length(NotFail)) {
+
+    BootData[, NotFail[i]] <- DataByObj[, NotFail[i]]
 
   }
 
-  boot.comp<-matrix(NA,ncol=4,nrow=R)
+  boot.comp <- matrix(NA, ncol = 4, nrow = R)
 
 
 
-  for (j in 1:R){
+  for (j in 1:R) {
 
-    if (NFC>0){
+    if (NFC > 0) {
 
-       SampleInd<-apply(matrix(rep(N,NFC),nrow=1),2,function(n) sample(1:n,n, replace=TRUE))
+      SampleInd <- apply(matrix(rep(N, NFC), nrow = 1), 2,
+                         function(n) sample(1:n, n, replace = TRUE))
 
-       for (k in 1:NFC) BootData[,FailCol[k]]<-DataByObj[SampleInd[,k],FailCol[k]]
+      for (k in 1:NFC) {
+        BootData[, FailCol[k]] <- DataByObj[SampleInd[, k], FailCol[k]]
+      }
 
-       boot.comp[j,]<-AllInd(BootData)
+      boot.comp[j, ] <- AllInd(BootData)
 
-       } else
-
-       boot.comp[j,]<-AllInd(DataByObj)
+    } else {
+      boot.comp[j, ] <- AllInd(DataByObj)
+    }
 
   }
 
@@ -136,41 +143,39 @@ BootIndex<-function(DataByObj, R=10000){
 
 
 
-BootCIRow<-function(DataObj, Column=FALSE, Par=4, CL=0.95, R=1000){
+BootCIRow <- function(DataObj, Column = FALSE, Par = 4, CL = 0.95, R = 1000) {
 
-#
+  #
 
-# DataObj : Data divided by objectices (accordingly)
+  # DataObj : Data divided by objectices (accordingly)
 
-# Column : Bootstrapping whether row or column-wise
+  # Column : Bootstrapping whether row or column-wise
 
-# Par    : Which parameter to consider.
+  # Par : Which parameter to consider.
 
-#          Par=1 (F1), Par=2 (F2), Par=3 (F3), Par=4 (WQI)
+  # Par=1 (F1), Par=2 (F2), Par=3 (F3), Par=4 (WQI)
 
-# CL     : Confidence coefficient
+  # CL : Confidence coefficient
 
-# R      : Number of bootstrap sample to consider
+  # R : Number of bootstrap sample to consider
 
-#
+  #
 
- if(Column) {
+  if (Column) {
 
-    BootCol<-BootIndex(DataObj, R=R)
+    BootCol <- BootIndex(DataObj, R = R)
 
-    CI<- quantile(BootCol[,Par], c((1-CL)/2, 1-(1-CL)/2))
+    CI <- quantile(BootCol[, Par], c((1 - CL)/2, 1 - (1 - CL)/2))
 
-    } else
+  } else {
 
-          {
+    BootRow <- boot(DataObj, statistic = AllInd, R = R)
 
-    BootRow<- boot(DataObj,statistic=AllInd,R=R)
+    CI <- quantile(BootRow$t[, Par], c((1 - CL)/2, 1 - (1 - CL)/2))
 
-    CI<- quantile(BootRow$t[,Par], c((1-CL)/2, 1-(1-CL)/2))
+  }
 
-    }
-
- CI
+  CI
 
 }
 
@@ -180,40 +185,38 @@ BootCIRow<-function(DataObj, Column=FALSE, Par=4, CL=0.95, R=1000){
 
 
 
-BootCICol<-function(DataObj, Column=TRUE, Par=4, CL=0.95, R=1000){
+BootCICol <- function(DataObj, Column = TRUE, Par = 4, CL = 0.95, R = 1000) {
 
-#
+  #
 
-# DataObj : Data divided by objectices (accordingly)
+  # DataObj : Data divided by objectices (accordingly)
 
-# Column : Bootstrapping whether row or column-wise
+  # Column : Bootstrapping whether row or column-wise
 
-# Par    : Which parameter to consider.
+  # Par : Which parameter to consider.
 
-#          Par=1 (F1), Par=2 (F2), Par=3 (F3), Par=4 (WQI)
+  # Par=1 (F1), Par=2 (F2), Par=3 (F3), Par=4 (WQI)
 
-# CL     : Confidence coefficient
+  # CL : Confidence coefficient
 
-# R      : Number of bootstrap sample to consider
+  # R : Number of bootstrap sample to consider
 
-#
+  #
 
- if(Column) {
+  if (Column) {
 
-    BootCol<-BootIndex(DataObj, R=R)
+    BootCol <- BootIndex(DataObj, R = R)
 
-    CICOL<- quantile(BootCol[,Par], c((1-CL)/2, 1-(1-CL)/2))
+    CICOL <- quantile(BootCol[, Par], c((1 - CL)/2, 1 - (1 - CL)/2))
 
-    } else
+  } else {
 
-          {
+    BootRow <- boot(DataObj, statistic = AllInd, R = R)
 
-    BootRow<- boot(DataObj,statistic=AllInd,R=R)
+    CICOL <- quantile(BootRow$t[, Par], c((1 - CL)/2, 1 - (1 - CL)/2))
 
-    CICOL<- quantile(BootRow$t[,Par], c((1-CL)/2, 1-(1-CL)/2))
+  }
 
-    }
-
- CICOL
+  CICOL
 
 }
