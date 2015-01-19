bfd780aefd1287b9513500e018fb9f009c2cd839
joethorley <joe@poissonconsulting.ca>
2015-01-19 07:17:42 -0800

    less packages required for fraser demo

--- demo/fraser.R
+++ demo/fraser.R
@@ -1,34 +1,24 @@
-library(magrittr)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(sp)
 library(rgdal)
-library(foreach)
-library(doParallel)
-
-if (getDoParWorkers() == 1) {
-  registerDoParallel(3)
-}
 
 data(fraser)
 
-fraser$Variable %<>% substitute_variables
-fraser$Units %<>% substitute_units
-fraser %<>% dplyr::filter(Variable %in% get_variables() & Units %in% get_units())
-fraser$SiteID %<>% as.character %>% sub("BC08", "", .) %>% factor
+fraser$Variable <- substitute_variables(fraser$Variable)
+fraser$Units <- substitute_units(fraser$Units)
+fraser <- dplyr::filter(fraser, Variable %in% get_variables() & Units %in% get_units())
+fraser$SiteID <-  factor(sub("BC08", "", as.character(fraser$SiteID)))
 
 plot_map(fraser, fill = "SiteID")
 
-fraser_limits <- calc_limits(fraser, by = c("SiteID", "Lat", "Long"),
-                             parallel = TRUE)
+fraser_limits <- calc_limits(fraser, by = c("SiteID", "Lat", "Long"))
 
-fraser_limits$Year <- year(fraser_limits$Date)
-fraser_wqis <- calc_wqis(fraser_limits, by = c("SiteID", "Year", "Lat", "Long"),
-                         parallel = TRUE)
+fraser_limits$Year <- lubridate::year(fraser_limits$Date)
+fraser_wqis <- calc_wqis(fraser_limits, by = c("SiteID", "Year", "Lat", "Long"))
 
-plot_wqis(fraser_wqis, x = "Year") + facet_wrap(~SiteID)
+plot_wqis(fraser_wqis, x = "Year") + ggplot2::facet_wrap(~SiteID)
 
 plot_map_wqis(filter(fraser_wqis, Year %in% c(1990, 2010)), drop = FALSE) +
-  facet_wrap(~Year, ncol = 1)
-
+  ggplot2::facet_wrap(~Year, ncol = 1)
