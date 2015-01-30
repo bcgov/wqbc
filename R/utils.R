ddb7be965635248c179a75a3b6473cb5a72b27c1
Joe Thorley <joe@poissonconsulting.ca>
2015-01-29 22:09:02 -0800

    moved geomean1 to utils

--- R/utils.R
+++ R/utils.R
@@ -91,3 +91,27 @@ proj_bc <- function (data, x, y, input_proj = NULL) {
 }
 
 is.error <- function (x) inherits (x, "try-error")
+
+#' Geometric Mean Plus-Minus 1
+#'
+#' Calculates geometric mean by adding 1 before logging
+#' and subtracting 1 before exponentiating so that
+#' geometric mean of
+#' @param x numeric vector of non-negative numbers
+#' @param na.rm flag indicating whether to remove missing values
+#' @return number
+#' @examples
+#' mean(0:9)
+#' geomean1(0:9)
+#' @export
+geomean1 <- function (x, na.rm = FALSE) {
+  assert_that(is.vector(x))
+  assert_that(is.flag(na.rm) && noNA(na.rm))
+  x <- as.numeric(x)
+
+  if(any(x < 0, na.rm = TRUE))
+    stop("x must not be negative")
+
+  expm1(mean(log1p(as.numeric(x)), na.rm = na.rm))
+}
+
