558c5a9e62ed76ddf0765e495cf4dc273610abb2
Joe Thorley <joe@poissonconsulting.ca>
2015-02-16 14:28:58 -0800

    start up msg

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("To the best of the package creators' knowledge the limits correspond to\nthe approved Provincial thresholds on the date of release\nbut wqbc comes with ABSOLUTELY NO WARRANTY.")
+}
