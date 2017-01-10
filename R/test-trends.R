eb2e5a449b03209fffa99c9a5022af06af1933ef
colin millar <colinpmillar@gmail.com>
2017-01-10 21:52:35 +0100

    test with prewhiting, also allow sen test with kendal p values

--- R/test-trends.R
+++ R/test-trends.R
@@ -10,14 +10,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-# test for a sen trend
-sen <- function(y, level = 0.95) {
+# test for a trend
+trend <- function(y, method = c("yuepilon", "zhang", "sen")) {
 
   # set up output structure
-  #cinames <- as.character(c((1-level)/2, 1 - (1-level)/2))
-  cinames <- c("ci_lower", "ci_upper")
-  out <- structure(rep(NA_real_, 3),
-                   names = c("est", cinames))
+  out <- structure(rep(NA_real_, 4),
+                   names = c("estimate", "lower", "upper", "sig"))
 
   # if there are less than four years do not run test
   if (sum(!is.na(y)) < 4) {
@@ -28,20 +26,31 @@ sen <- function(y, level = 0.95) {
   Year <- seq_along(y)[!is.na(y)]
   y <- y[!is.na(y)]
 
-  # estimate sen slope
-  ss <- zyp::zyp.sen(y ~ Year)
-  # calculate confidence intervals
-  ss.ci <- zyp::confint.zyp(ss, level = level)
-
-  # fill in estimate and confidence interval for slope
-  out["est"] <- stats::coef(ss)["Year"]
-  out[cinames] <- ss.ci["Year",]
+  if (method == "sen") {
+    # estimate sen slope
+    ss <- zyp::zyp.sen(y ~ Year)
+    # calculate confidence intervals
+    ss.ci <- zyp::confint.zyp(ss, level = 0.95)
+    # fill in estimate and confidence interval for slope
+    out[1] <- stats::coef(ss)["Year"]
+    out[2:3] <- ss.ci["Year",]
+    out[4] <- Kendall::Kendall(y, Year)$sl
+  } else
+  if (method %in% c("yuepilon", "zhang")) {
+    zs <- zyp::zyp.trend.vector(y, x = Year, method = method,
+                                conf.intervals = TRUE,
+                                preserve.range.for.sig.test = TRUE)
+    out[] <- zs[c("trend", "lbound", "ubound", "sig")]
+  } else {
+    stop("Unknown method :", method)
+  }
 
   # return
   out
 }
 
-do_test_trends <- function(data, breaks = NULL, FUN = "median", level = 0.95) {
+
+do_test_trends <- function(data, breaks, FUN, method) {
 
   # get function to use for summarise multiple observations
   FUN <- match.fun(FUN)
@@ -63,7 +72,7 @@ do_test_trends <- function(data, breaks = NULL, FUN = "median", level = 0.95) {
 
   # fit trend test by month grouping and return
   groups <- colnames(data)
-  data %<>% apply(MARGIN = 2, sen, level = level) %>%
+  data %<>% apply(MARGIN = 2, trend, method = method) %>%
             t() %>%
             tibble::as_data_frame()
   data$months <- groups
@@ -73,7 +82,7 @@ do_test_trends <- function(data, breaks = NULL, FUN = "median", level = 0.95) {
 
 
 
-#' Thiel-Sen Trend Teste
+#' Thiel-Sen Trend Test
 #'
 #' Analyses time series using the Thiel-Sen estimate of slope.
 #'
@@ -83,19 +92,28 @@ do_test_trends <- function(data, breaks = NULL, FUN = "median", level = 0.95) {
 #' @param breaks A numeric vector used to create groups of consecutive months, if NULL the full
 #'               year is used.
 #' @param FUN The function to use for yearly summaries, e.g. median, mean, or max.
-#' @param level The confidence level to compute a confidence interval for the Thiel-Sen slope.
+#' @param method The method to use.
 #' @param messages A flag indicating whether to print messages.
+#'
 #' @return A tibble data.frame with rows for each Station Variable, and month grouping, and
 #'         additional columns for the slope estinate and confidence intervals.
+#' @details
+#'
+#' This routine computes a prewhitened nonlinear trend on a vector of data, using either
+#' Zhang's (described in Wang and Swail, 2001) or Yue Pilon's (describe in Yue Pilon, 2002)
+#' method of prewhitening and Sen's slope, and use a Kendall test for significance.
+#'
 #' @examples
 #'  data <- wqbc::yuepilon
 #'  trend <- test_trends(data, breaks = 6, messages = TRUE)
+#'  trend <- test_trends(data, breaks = 6, messages = TRUE, method = "sen")
 #' @export
-test_trends <- function(data, breaks = NULL, FUN = "median", level = 0.95,
+test_trends <- function(data, breaks = NULL, FUN = "median", method = "yuepilon",
                         messages = getOption("wqbc.messages", default = TRUE)) {
 
   # check inputs
   check_flag(messages)
+  match.arg(method, c("yuepilon", "zhang", "sen"))
   check_cols(data, c("Station", "Date", "Variable", "Value", "Units"))
   check_data2(data, list(Date = Sys.Date(),
                          Value = c(1, NA)))
@@ -108,7 +126,7 @@ test_trends <- function(data, breaks = NULL, FUN = "median", level = 0.95,
 
   # fit trends
   data %<>% dplyr::mutate_(Trend = ~purrr::map(Data, do_test_trends,
-                                               breaks = breaks, FUN = FUN, level = level))
+                                               breaks = breaks, FUN = FUN, method = method))
 
   # unnest and return
   data %>% tidyr::unnest_(unnest_cols = c("Trend"), .drop = TRUE)
--- tests/testthat/test-test-trends.R
+++ tests/testthat/test-test-trends.R
@@ -14,9 +14,18 @@ context("test-trends")
 
 test_that("test_trends", {
   expect_error(test_trends(wqbc::dummy))
-  yuepilon <- wqbc::yuepilon
-  trend <- test_trends(yuepilon)
+  trend <- test_trends(wqbc::yuepilon)
   expect_identical(nrow(trend), 4L)
-  expect_identical(ncol(trend), 7L)
-  expect_is(trend$est, "numeric")
+  expect_identical(ncol(trend), 8L)
+  expect_is(trend$estimate, "numeric")
+  expect_is(trend$months, "character")
+  expect_lt(abs(trend$estimate[1] - 0.01350760), 1e-6)
 })
+
+
+# test values
+# test breaks
+# sanity check
+
+
+
