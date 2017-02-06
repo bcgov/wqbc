26afbe61944474bda2ca64de9ece2efec23485c8
colin millar <colinpmillar@gmail.com>
2017-02-06 22:47:19 +0100

    fixed a few small bugs and added error trapping for small datasets

--- R/model-totalhardness.R
+++ R/model-totalhardness.R
@@ -15,44 +15,49 @@ model_totalhardness_by <- function(x, messages) {
 
   # filter out total hardness observations
   x_ht <- x %>% dplyr::filter_(~Variable == "Hardness Total")
-  
+
   # identify all sampled dates in data and join onto x_ht data
   # it is nessisary to include Station as this cannot be guessed
-  x_ht <- x %>% dplyr::select_(~Date,~Station) %>% 
+  x_ht <- x %>% dplyr::select_(~Date,~Station) %>%
                 unique(.) %>%
                 dplyr::left_join(x_ht, by = c("Date", "Station"))
   x_ht$Units[is.na(x_ht$Units)] <- x_ht$Units[1]
   x_ht$Variable[is.na(x_ht$Variable)] <- "Hardness Total"
-  
-  # extract seasonal covariates from 'Date'
-  x_ht %<>% dplyr::mutate_(yday = ~lubridate::yday(Date), # for seasonal trend
-                           day = ~lubridate::decimal_date(Date)) # for long term trends
 
-  # step 1: long term trend, set k to number of years
-  #         (could possibly go up to 1.5 * no. of years)
-  # note - also fit a seasonal model to allow for unbalanced sampling
-  #        within some years.
-  k_t <- diff(range(lubridate::year(x_ht$Date))) + 1L
-  trend_f <- sprintf("Value ~ s(day, k = %i) + s(yday, bs = 'cc', k = 6)", as.integer(k_t))
-  mod1 <- mgcv::gam(stats::formula(trend_f), data = x_ht)
-  ## set yday coeffs to zero to remove them from the model
-  mod1$coefficients[grep("yday", names(mod1$coefficients))] <- 0
-  x_ht$Value_trend <- mgcv::predict.gam(mod1, newdata = x_ht)
+  # if there is not enough observations, then skip modelling and return with NAs
+  # here defined as 6 x the year range
+  if (sum(!is.na(x_ht$Value)) > (diff(range(lubridate::year(x_ht$Date))) + 1) * 6 ) {
+    # extract seasonal covariates from 'Date'
+    x_ht %<>% dplyr::mutate_(yday = ~lubridate::yday(Date), # for seasonal trend
+                              day = ~lubridate::decimal_date(Date)) # for long term trends
 
-  # step 2: model seasonal trend component:  allow seasonal trend to evolve.
-  seasonal_f <- sprintf("I(Value - Value_trend) ~ te(yday, day, bs = c('cc', 'tp'), k = c(6, %i))", k_t)
-  mod2 <- mgcv::gam(stats::formula(seasonal_f),
-                    data = x_ht)
-  x_ht$Value_season <- mgcv::predict.gam(mod2, newdata = x_ht)
+    # step 1: long term trend, set k to number of years
+    #         (could possibly go up to 1.5 * no. of years)
+    # note - also fit a seasonal model to allow for unbalanced sampling
+    #        within some years.
+    k_t <- diff(range(lubridate::year(x_ht$Date))) + 1L
+    trend_f <- sprintf("Value ~ s(day, k = %i) + s(yday, bs = 'cc', k = 6)", as.integer(k_t))
+    mod1 <- mgcv::gam(stats::formula(trend_f), data = x_ht)
+    ## set yday coeffs to zero to remove them from the model
+    mod1$coefficients[grep("yday", names(mod1$coefficients))] <- 0
+    x_ht$Value_trend <- mgcv::predict.gam(mod1, newdata = x_ht)
 
-  # replace Values with modelled ones
-  x_ht %<>% dplyr::mutate_(Value = ~Value_trend + Value_season)
+    # step 2: model seasonal trend component:  allow seasonal trend to evolve.
+    seasonal_f <- sprintf("I(Value - Value_trend) ~ te(yday, day, bs = c('cc', 'tp'), k = c(6, %i))", k_t)
+    mod2 <- mgcv::gam(stats::formula(seasonal_f), data = x_ht)
+    x_ht$Value_season <- mgcv::predict.gam(mod2, newdata = x_ht)
 
-  # remove working columns
-  x_ht %<>% dplyr::select_(~-Value_season, ~-Value_trend, ~-yday, ~-day)
+    # replace Values with modelled ones
+    x_ht %<>% dplyr::mutate_(Value = ~Value_trend + Value_season)
 
+    # remove working columns
+    x_ht %<>% dplyr::select_(~-Value_season, ~-Value_trend, ~-yday, ~-day)
+  } else {
+    # not enough non-NA observations
+  }
   # join back onto full data and return
-  dplyr::full_join(x, x_ht)
+  x %>% dplyr::filter_(~Variable != "Hardness Total") %>%
+        dplyr::full_join(x_ht, by = names(x))
 }
 
 model_totalhardness <- function(data, by = NULL,
