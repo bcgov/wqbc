9c2994154108afe9ef9f8245528b69a547c46a6a
colin millar <colinpmillar@gmail.com>
2017-02-07 21:26:06 +0100

    include seperate model if only one year of data

--- R/model-totalhardness.R
+++ R/model-totalhardness.R
@@ -16,45 +16,65 @@ model_totalhardness_by <- function(x, messages) {
   # filter out total hardness observations
   x_ht <- x %>% dplyr::filter_(~Variable == "Hardness Total")
 
+  # filter out everything other than hardness total
+  x %<>% dplyr::filter_(~Variable != "Hardness Total")
+
   # identify all sampled dates in data and join onto x_ht data
   # it is nessisary to include Station as this cannot be guessed
-  x_ht <- x %>% dplyr::select_(~Date,~Station) %>%
+  x_ht <- x %>% dplyr::select_(~Date, ~Station) %>%
                 unique(.) %>%
-                dplyr::left_join(x_ht, by = c("Date", "Station"))
-  x_ht$Units[is.na(x_ht$Units)] <- x_ht$Units[1]
-  x_ht$Variable[is.na(x_ht$Variable)] <- "Hardness Total"
+          dplyr::left_join(x_ht, by = c("Date", "Station"))
+  # fill in Units and Variable with Hardness Total values
+  x_ht$Units <- x_ht$Units[1]
+  x_ht$Variable <- "Hardness Total"
 
-  # if there is not enough observations, then skip modelling and return with NAs
+  # if there is not enough observations, then
+  # skip modelling and return with NAs
   # here defined as 6 x the year range
-  if (sum(!is.na(x_ht$Value)) > (diff(range(lubridate::year(x_ht$Date))) + 1) * 6 ) {
+  ndata_years <- x_ht %>%
+                   dplyr::filter_(~!is.na(Value)) %>%
+                   dplyr::mutate_(year = ~lubridate::year(Date)) %>%
+                   dplyr::group_by_(~year) %>%
+                   dplyr::tally()
+  ndata_years <- sum(ndata_years$n > 6)
+  if (ndata_years == 0) {
+    if (messages) message("Not enough Hardness Total observations to allow modelling.")
+  } else {
     # extract seasonal covariates from 'Date'
     x_ht %<>% dplyr::mutate_(yday = ~lubridate::yday(Date), # for seasonal trend
                               day = ~lubridate::decimal_date(Date)) # for long term trends
 
-    # step 1: long term trend, set k to number of years
-    #         (could possibly go up to 1.5 * no. of years)
-    # note - also fit a seasonal model to allow for unbalanced sampling
-    #        within some years.
-    k_t <- diff(range(lubridate::year(x_ht$Date))) + 1L
-    trend_f <- sprintf("Value ~ s(day, k = %i) + s(yday, bs = 'cc', k = 6)", as.integer(k_t))
-    mod1 <- mgcv::gam(stats::formula(trend_f), data = x_ht)
-    ## set yday coeffs to zero to remove them from the model
-    mod1$coefficients[grep("yday", names(mod1$coefficients))] <- 0
-    x_ht$Value_trend <- mgcv::predict.gam(mod1, newdata = x_ht)
+    if (ndata_years == 1) {
+      # simply fit a smoother
+      mod <- mgcv::gam(Value ~ yday + s(yday, k = 6, bs = "cc"), data = x_ht)
 
-    # step 2: model seasonal trend component:  allow seasonal trend to evolve.
-    seasonal_f <- sprintf("I(Value - Value_trend) ~ te(yday, day, bs = c('cc', 'tp'), k = c(6, %i))", k_t)
-    mod2 <- mgcv::gam(stats::formula(seasonal_f), data = x_ht)
-    x_ht$Value_season <- mgcv::predict.gam(mod2, newdata = x_ht)
+      # replace Values with modelled ones
+      x_ht$Value <- mgcv::predict.gam(mod, newdata = x_ht)
+      
+      # remove working columns
+      x_ht %<>% dplyr::select_(~-Value_season, ~-Value_trend, ~-yday, ~-day)
+    } else {
+      # step 1: long term trend.
+      k_t <- ndata_years
+      trend_f <- sprintf("Value ~ s(day, k = %i) + s(yday, bs = 'cc', k = 6)", as.integer(k_t))
+      mod1 <- mgcv::gam(stats::formula(trend_f), data = x_ht)
+      ## set yday coeffs to zero to remove them from the model
+      mod1$coefficients[grep("yday", names(mod1$coefficients))] <- 0
+      x_ht$Value_trend <- mgcv::predict.gam(mod1, newdata = x_ht)
 
-    # replace Values with modelled ones
-    x_ht %<>% dplyr::mutate_(Value = ~Value_trend + Value_season)
+      # step 2: model seasonal trend component:  allow seasonal trend to evolve.
+      seasonal_f <- sprintf("I(Value - Value_trend) ~ te(yday, day, bs = c('cc', 'tp'), k = c(6, %i))", k_t)
+      mod2 <- mgcv::gam(stats::formula(seasonal_f), data = x_ht)
+      x_ht$Value_season <- mgcv::predict.gam(mod2, newdata = x_ht)
 
-    # remove working columns
-    x_ht %<>% dplyr::select_(~-Value_season, ~-Value_trend, ~-yday, ~-day)
-  } else {
-    # not enough non-NA observations
+      # replace Values with modelled ones
+      x_ht %<>% dplyr::mutate_(Value = ~Value_trend + Value_season)
+
+      # remove working columns
+      x_ht %<>% dplyr::select_(~-Value_season, ~-Value_trend, ~-yday, ~-day)
+    }
   }
+
   # join back onto full data and return
   x %>% dplyr::filter_(~Variable != "Hardness Total") %>%
         dplyr::full_join(x_ht, by = names(x))
@@ -63,11 +83,12 @@ model_totalhardness_by <- function(x, messages) {
 model_totalhardness <- function(data, by = NULL,
                                 messages = getOption("wqbc.messages", default = TRUE)) {
 
-  # check the contents of data, all we require is Value, Date
-  # ... and by variables ...
+  # check the contents of data
   check_data2(data, values = list(Value = c(1, NA),
                                   Date = as.Date("2014/1/1"),
-                                  Variable = ""))
+                                  Station = factor(""),
+                                  Variable = "",
+                                  Units = ""))
   if (is.null(by)) {
     data %<>% model_totalhardness_by(messages = messages)
   } else {
