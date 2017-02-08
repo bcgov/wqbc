ac40c55033bbd9841de9ce614417fbcef9ff052b
joethorley <joe@poissonconsulting.ca>
2017-02-08 15:02:16 -0800

    just sen slope and significance

--- R/test-trends.R
+++ R/test-trends.R
@@ -11,16 +11,15 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 # test for a trend
-trend <- function(y, prewhiten = c("zhang", "yuepilon", "none")) {
+trend <- function(y) {
 
   # set up output structure
   out <- dplyr::data_frame(
-    sen_slope = NA_real_,
-    sen_slope_lower = NA_real_,
-    sen_slope_upper = NA_real_,
-    sen_intercept = NA_real_,
-    sen_slope_sig = NA,
-    kendall_sig = NA)
+    slope = NA_real_,
+    lower = NA_real_,
+    upper = NA_real_,
+    intercept = NA_real_,
+    significance = NA)
 
   # if there are less than six years do not run test
   if (sum(!is.na(y)) <= 5) {
@@ -31,35 +30,26 @@ trend <- function(y, prewhiten = c("zhang", "yuepilon", "none")) {
   Year <- seq_along(y)[!is.na(y)]
   y <- y[!is.na(y)]
 
-  if (prewhiten == "none") {
-    # estimate sen slope
-    ss <- zyp::zyp.sen(y ~ Year)
-    # calculate confidence intervals
-    ss.ci <- zyp::confint.zyp(ss, level = 0.95)
-    # fill in estimate and confidence interval for slope
-    out[1] <- stats::coef(ss)["Year"]
-    out[2:3] <- ss.ci["Year",]
-    out[4] <- stats::coef(ss)["Intercept"]
-    out[6] <- Kendall::Kendall(y, Year)$sl
-  } else {
-    zs <- zyp::zyp.trend.vector(y, x = Year, method = prewhiten,
-                                conf.intervals = TRUE,
-                                preserve.range.for.sig.test = TRUE)
-    out[c(1:4,6)] <- zs[c("trend", "lbound", "ubound", "intercept", "sig")]
-  }
+  # estimate sen slope
+  ss <- zyp::zyp.sen(y ~ Year)
+  # calculate confidence intervals
+  ss.ci <- zyp::confint.zyp(ss, level = 0.95)
+  # fill in estimate and confidence interval for slope
+  out[1] <- stats::coef(ss)["Year"]
+  out[2:3] <- ss.ci["Year",]
+  out[4] <- stats::coef(ss)["Intercept"]
   out[5] <- !(out[2] <= 0 & out[3] >= 0)
-  out[6] <- out[6] <= 0.05
   out
 }
 
-do_test_trends <- function(data, breaks, FUN, prewhiten) {
+do_test_trends <- function(data, breaks, FUN) {
 
   # Summarise data
   data %<>% do_summarise_for_trends(breaks, FUN, return_year = FALSE)
 
   data %<>% as.data.frame() #%>% tidyr::gather_("Month", "Value", gather_cols = colnames(data), na.rm = TRUE)
 
-  data %<>% purrr::map(trend, prewhiten = prewhiten)
+  data %<>% purrr::map(trend)
 
   groups <- names(data)
   data %<>% dplyr::bind_rows()
@@ -78,28 +68,11 @@ do_test_trends <- function(data, breaks, FUN, prewhiten) {
 #' @param breaks A numeric vector used to create groups of consecutive months, if NULL the full
 #'               year is used.
 #' @param FUN The function to use for yearly summaries, e.g. median, mean, or max.
-#' @param prewhiten A string of the method to use for prewhitening. Values must be 'yuepilon', 'zhang' or 'none'.
 #' @param messages A flag indicating whether to print messages.
 #' @return A tibble data.frame with rows for each Station, Variable, and month grouping, and
-#'         additional columns for the sen slope estinate, 95\% confidence intervals on the slope, intercept and significance at the 5\% level for both the sen slope and Kendall tau (which can differ).
-#' @seealso \code{\link[zyp]{zyp.yuepilon}}, \code{\link[zyp]{zyp.zhang}}, \code{\link[zyp]{zyp.sen}}
-#'   and \code{\link[Kendall]{Kendall}} for the individual methods implmented.
+#'         additional columns for the sen slope estinate, 95\% confidence intervals on the slope, intercept and significance at the 5\% level for the sen slope.
+#' @seealso \code{\link[zyp]{zyp.sen}}
 #' @references
-#'
-#' Yue, S., Pilon, P., Phinney, B., and Cavadias, G. (2002). The influence of autocorrelation on the
-#' ability to detect trend in hydrological series. Hydrological Processes, 16(9), 1807-1829.
-#'
-#' Zhang, X., Harvey, K. D., Hogg, W. D., and Yuzyk, T. R. (2001). Trends in Canadian streamflow.
-#' Water Resources Research, 37(4), 987-998.
-#'
-#' Kendall, M. (1938). A New Measure of Rank Correlation. Biometrika. 30 (1-2): 81-89.
-#' doi:10.1093/biomet/30.1-2.81
-#'
-#' Theil, H. (1950), A rank-invariant method of linear and polynomial regression analysis. I, II,
-#' III, Nederl. Akad. Wetensch., Proc., 53: 386-392, 521-525, 1397-1412.
-#'
-#' Sen, Pranab Kumar (1968), Estimates of the regression coefficient based on Kendall's tau,
-#' Journal of the American Statistical Association, 63 (324): 1379-1389, doi:10.2307/2285891
 #'
 #' @examples
 #'  data <- wqbc::yuepilon
@@ -108,13 +81,10 @@ do_test_trends <- function(data, breaks, FUN, prewhiten) {
 #'   demo(test_trends)
 #' }
 #' @export
-test_trends <- function(data, breaks = NULL, FUN = "median", prewhiten = "zhang",
-                        messages = getOption("wqbc.messages", default = TRUE)) {
+test_trends <- function(data, breaks = NULL, FUN = "median", messages = getOption("wqbc.messages", default = TRUE)) {
 
   # check inputs
   check_flag(messages)
-  check_string(prewhiten)
-  if (!prewhiten %in% c("yuepilon", "zhang", "none")) error("prewhiten must be 'yuepilon', 'zhang' or 'none'")
 
   check_cols(data, c("Station", "Date", "Variable", "Value", "Units"))
   check_data2(data, list(Date = Sys.Date(),
@@ -128,7 +98,7 @@ test_trends <- function(data, breaks = NULL, FUN = "median", prewhiten = "zhang"
 
   # fit trends
   data %<>% dplyr::mutate_(Trend = ~purrr::map(Data, do_test_trends,
-                                               breaks = breaks, FUN = FUN, prewhiten = prewhiten))
+                                               breaks = breaks, FUN = FUN))
 
   # unnest and return
   data %>% tidyr::unnest_(unnest_cols = c("Trend"), .drop = TRUE)
@@ -180,16 +150,13 @@ do_summarise_for_trends <- function(data, breaks, FUN, return_year = TRUE) {
 #'  data(yuepilon)
 #'  data <- yuepilon[yuepilon$Station == "02EA005", ]
 #'  # estimate trend (using simple sen slope)
-#'  trend <- test_trends(data, messages = TRUE, prewhiten = "none")
+#'  trend <- test_trends(data, messages = TRUE)
 #'  # get the data used in the test
 #'  datasum <- summarise_for_trends(data)
 #'  plot(datasum$Year, datasum$Value,
-#'       main = paste("p-value =", round(trend$sen_slope_sig, 3)),
+#'       main = paste("p-value =", round(trend$significance, 3)),
 #'       ylab = "Value", xlab = "Year", las = 1)
 #' @export
-
-# data <- ems %>% rename(Station = Station_Number) %>% filter(Station == "BC08HB0018");breaks = 6; FUN <- median
-
 summarise_for_trends <- function(data, breaks = NULL, FUN = "median",
                                  messages = getOption("wqbc.messages", default = TRUE)) {
 
