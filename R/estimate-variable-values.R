c6075ba5949ef869baba4272e107038e75b47629
joethorley <joe@poissonconsulting.ca>
2017-02-08 06:40:04 -0800

    extend estimate-variable-values to Chloride Total and pH

--- R/estimate-variable-values.R
+++ R/estimate-variable-values.R
@@ -13,101 +13,108 @@
 
 estimate_variable_values_by <- function(x, messages) {
 
-  # filter out total hardness observations
-  x_ht <- x %>% dplyr::filter_(~Variable == "Hardness Total")
-  if (nrow(x_ht) == 0) {
-    if (messages) message("No Hardness Total observations so returning origonal data.")
-    return(x)
-  }
-
-  # filter out everything other than hardness total
-  x %<>% dplyr::filter_(~Variable != "Hardness Total")
-
-  # identify all sampled dates in data and join onto x_ht data
-  # it is nessisary to include Station as this cannot be guessed
-  x_ht <- x %>% dplyr::select_(~Date, ~Station) %>%
-                unique(.) %>%
-          dplyr::full_join(x_ht, by = c("Date", "Station"))
-  # fill in Units and Variable with Hardness Total values
-  x_ht$Units <- x_ht$Units[1]
-  x_ht$Variable <- "Hardness Total"
-
   # if there is not enough observations, then
   # skip modelling and return with NAs
   # here defined as 6 x the year range
-  ndata_years <- x_ht %>%
-                   dplyr::filter_(~!is.na(Value)) %>%
-                   dplyr::mutate_(year = ~lubridate::year(Date)) %>%
-                   dplyr::group_by_(~year) %>%
-                   dplyr::tally()
+  ndata_years <- x %>%
+    dplyr::filter_(~!is.na(Value)) %>%
+    dplyr::mutate_(year = ~lubridate::year(Date)) %>%
+    dplyr::group_by_(~year) %>%
+    dplyr::tally()
   ndata_years <- sum(ndata_years$n >= 12)
   if (ndata_years == 0) {
-    if (messages) message("Not enough Hardness Total observations to allow modelling, using mean.")
-    x_ht$Value <- mean(x_ht$Value, na.rm = TRUE)
+    if (messages) message("Insufficient observations to allow modelling, using mean.")
+    x$Value <- mean(x$Value, na.rm = TRUE)
   } else {
     # extract seasonal covariates from 'Date'
-    x_ht %<>% dplyr::mutate_(yday = ~lubridate::yday(Date), # for seasonal trend
-                              day = ~lubridate::decimal_date(Date)) # for long term trends
+    x %<>% dplyr::mutate_(yday = ~lubridate::yday(Date), # for seasonal trend
+                          day = ~lubridate::decimal_date(Date)) # for long term trends
 
     if (ndata_years == 1) {
       # simply fit a seasonal smoother
-      mod <- mgcv::gam(Value ~ s(yday, k = 6, bs = "cc"), data = x_ht)
+      mod <- mgcv::gam(Value ~ s(yday, k = 6, bs = "cc"), data = x)
 
       # replace Values with modelled ones
-      x_ht$Value <- mgcv::predict.gam(mod, newdata = x_ht)
+      x$Value <- mgcv::predict.gam(mod, newdata = x)
     }
     if (ndata_years == 2) {
       # simply fit a seasonal smoother with a trend
-      mod <- mgcv::gam(Value ~ s(day, k=3) + s(yday, k = 6, bs = "cc"), data = x_ht)
+      mod <- mgcv::gam(Value ~ s(day, k=3) + s(yday, k = 6, bs = "cc"), data = x)
 
       # replace Values with modelled ones
-      x_ht$Value <- mgcv::predict.gam(mod, newdata = x_ht)
+      x$Value <- mgcv::predict.gam(mod, newdata = x)
     }
     if (ndata_years > 2) {
       # fit long term trend and seasonally evolving trend in 2 steps
       # step 1: long term trend.
       k_t <- ndata_years
       trend_f <- sprintf("Value ~ s(day, k = %i) + s(yday, bs = 'cc', k = 6)", as.integer(k_t))
-      mod1 <- mgcv::gam(stats::formula(trend_f), data = x_ht)
+      mod1 <- mgcv::gam(stats::formula(trend_f), data = x)
       ## set yday coeffs to zero to remove them from the model
       mod1$coefficients[grep("yday", names(mod1$coefficients))] <- 0
-      x_ht$Value <- x_ht$Value - mgcv::predict.gam(mod1, newdata = x_ht)
+      x$Value <- x$Value - mgcv::predict.gam(mod1, newdata = x)
 
       # step 2: model seasonal trend component:  allow seasonal trend to evolve.
       seasonal_f <- sprintf("Value ~ te(yday, day, bs = c('cc', 'tp'), k = c(6, %i))", k_t)
-      mod2 <- mgcv::gam(stats::formula(seasonal_f), data = x_ht)
+      mod2 <- mgcv::gam(stats::formula(seasonal_f), data = x)
 
       # replace Values with modelled ones
-      x_ht$Value <- mgcv::predict.gam(mod1, newdata = x_ht) +
-                    mgcv::predict.gam(mod2, newdata = x_ht)
+      x$Value <- mgcv::predict.gam(mod1, newdata = x) +
+        mgcv::predict.gam(mod2, newdata = x)
     }
 
     # remove working columns
-    x_ht %<>% dplyr::select_(~-yday, ~-day)
+    x %<>% dplyr::select_(~-yday, ~-day)
   }
-
-  # join back onto full data and return
-  x %>% dplyr::filter_(~Variable != "Hardness Total") %>%
-        dplyr::full_join(x_ht, by = names(x))
+  x
 }
 
-estimate_variable_values <- function(data, by = NULL,
-                                messages = getOption("wqbc.messages", default = TRUE)) {
+estimate_variable_values <- function(data, by = NULL, variables = "Hardness Total",
+                                     messages = getOption("wqbc.messages", default = TRUE)) {
 
-  # check the contents of data
-  check_data2(data, values = list(Value = c(1, NA),
-                                  Date = as.Date("2014/1/1"),
-                                  Station = factor(""),
-                                  Variable = "",
-                                  Units = ""))
-  if (is.null(by)) {
-    data %<>% estimate_variable_values_by(messages = messages)
-  } else {
-    data %<>% plyr::ddply(.variables = by, .fun = estimate_variable_values_by,
-                          messages = messages)
+  check_data2(data, values = list(Date = Sys.Date(), Variable = "", Value = c(1, NA), Units = ""))
+  assert_that(is.null(by) || (is.character(by) && noNA(by)))
+  check_vector(variables, value = "")
+  if (!all(variables %in% c("Chloride Total", "Hardness Total", "pH"))) error("Unrecognized variables")
+  check_flag(messages)
+  stopifnot(!any(c("Variable", "Value", "Units", "Date") %in% by))
+
+  variables <- unique(variables)
+
+  dates_by <- data[unique(c("Date", by))] %>% dplyr::distinct()
+
+  for (variable in variables) {
+
+    if (messages) message("Estimating ", variable)
+
+    new_data <- dplyr::filter_(data, ~Variable == variable)
+
+    if (nrow(new_data)) {
+
+      new_data %<>% standardize_wqdata(messages = messages)
+
+      unit <- new_data$Units[1]
+
+      new_data %<>% dplyr::right_join(dates_by, by = c("Date", by))
+
+      new_data$Units <- unit
+      new_data$Variable <- variable
+
+      if (is.null(by)) {
+        new_data %<>% estimate_variable_values_by(messages = messages)
+      } else {
+        new_data %<>% plyr::ddply(.variables = by, .fun = estimate_variable_values_by,
+                                  messages = messages)
+      }
+
+      data %<>% dplyr::filter_(~!Variable == variable)
+
+      data %<>% dplyr::bind_rows(new_data)
+
+      if (messages) message("Estimated ", variable, " values")
+
+    } else if (messages) message("No ", variable, " values")
   }
-
-  if (messages) message("Replaced water hardness observations with modelled values.")
 
   data
 }
--- tests/testthat/test-estimate-variable-values.R
+++ tests/testthat/test-estimate-variable-values.R
@@ -13,11 +13,13 @@
 context("model-totalhardness")
 
 test_that("model-totalhardness vary data", {
-data(fraser)
+fraser <- wqbc::fraser
 fraser$Station <-  factor(sub("BC08", "", as.character(fraser$SiteID)))
 fraser <- dplyr::filter_(fraser, ~grepl("hardness", tolower(Variable)))
+
 fraser <- dplyr::filter_(fraser, ~Station %in% "LF0001")
 fraser <- clean_wqdata(fraser, by = "Station", message = FALSE)
+
 
 # create degraded data
 fraser2 <- dplyr::filter_(fraser, ~lubridate::year(Date) %in% 2012:2013)
@@ -28,28 +30,33 @@ fraser1b <- dplyr::mutate_(fraser1, Value = ~ifelse(1:nrow(fraser1) %% 2 == 0, N
 fraser1c <- dplyr::mutate(fraser1, Variable = "Nothing")
 
 # fit models
-fit   <- estimate_variable_values(fraser,   by = "Station", message = FALSE)$Value
-fit2  <- estimate_variable_values(fraser2,  by = "Station", message = FALSE)$Value
-fit2a <- estimate_variable_values(fraser2a, by = "Station", message = FALSE)$Value
-fit1  <- estimate_variable_values(fraser1,  by = "Station", message = FALSE)$Value
-fit1a <- estimate_variable_values(fraser1a, by = "Station", message = FALSE)$Value
-fit1b <- estimate_variable_values(fraser1b, by = "Station", message = FALSE)$Value
+fit   <- estimate_variable_values(fraser,   by = "Station", messages = FALSE)
+
+expect_identical(colnames(fit), colnames(fraser))
+expect_identical(nrow(fit), nrow(fraser))
+
+fit2  <- estimate_variable_values(fraser2,  by = "Station", message = FALSE)
+fit2a <- estimate_variable_values(fraser2a, by = "Station", message = FALSE)
+fit1  <- estimate_variable_values(fraser1,  by = "Station", message = FALSE)
+fit1a <- estimate_variable_values(fraser1a, by = "Station", message = FALSE)
+fit1b <- estimate_variable_values(fraser1b, by = "Station", message = FALSE)
 
 # check predictions are near origional values
 # max % abs diff
-expect_lt(max(abs(fit/fraser$Value - 1)), 0.32)
-expect_lt(max(abs(fit2/fraser2$Value - 1)), 0.25)
-expect_lt(max(abs(fit2a/fraser2$Value - 1)), 0.25)
-expect_lt(max(abs(fit1/fraser1$Value - 1)), 0.25)
-expect_lt(max(abs(fit1b/fraser1$Value - 1)), 0.25)
+expect_lt(max(abs(fit$Value/fraser$Value - 1)), 0.32)
+expect_lt(max(abs(fit2$Value/fraser2$Value - 1)), 0.25)
+expect_lt(max(abs(fit2a$Value[!is.na(fraser2a$Value)]/fraser2a$Value[!is.na(fraser2a$Value)] - 1)), 0.25)
+expect_true(all(!is.na(fit2a$Value)))
+expect_lt(max(abs(fit1$Value/fraser1$Value - 1)), 0.25)
+expect_lt(max(abs(fit1b$Value/fraser1$Value - 1)), 0.25)
 # mean % abs diff
-expect_lt(mean(abs(fit/fraser$Value - 1)), 0.05)
-expect_lt(mean(abs(fit2/fraser2$Value - 1)), 0.05)
-expect_lt(mean(abs(fit2a/fraser2$Value - 1)), 0.05)
-expect_lt(mean(abs(fit1/fraser1$Value - 1)), 0.05)
-expect_lt(mean(abs(fit1b/fraser1$Value - 1)), 0.06)
+expect_lt(mean(abs(fit$Value/fraser$Value - 1)), 0.05)
+expect_lt(mean(abs(fit2$Value/fraser2$Value - 1)), 0.05)
+expect_lt(mean(abs(fit2a$Value/fraser2$Value - 1)), 0.05)
+expect_lt(mean(abs(fit1$Value/fraser1$Value - 1)), 0.05)
+expect_lt(mean(abs(fit1b$Value/fraser1$Value - 1)), 0.06)
 # should give mean
-expect_equal(fit1a[1], mean(fraser1a$Value, na.rm = TRUE))
+expect_equal(fit1a$Value[1], mean(fraser1a$Value, na.rm = TRUE))
 
 # test inputs
 expect_error(estimate_variable_values(select_(fraser1, ~-Station)))
