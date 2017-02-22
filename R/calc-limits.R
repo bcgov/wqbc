e6a57ff4e81aed32c7424057f148880fdb046065
joethorley <joe@poissonconsulting.ca>
2017-02-22 09:39:22 -0800

    tweak calc-limits documentation

--- R/calc-limits.R
+++ R/calc-limits.R
@@ -230,7 +230,7 @@ calc_limits_by <- function (x, term, dates, limits, messages) {
 #' The one exception is if \code{estimate_variables = TRUE} in which case a parametric model
 #' is used to predict the pH, Total Chloride and Total Hardness for all dates with a value of any variable.
 #' Existing values are replaced.
-#' If there are less then 12 pH/Total Chloride/Total Hardness values in all years then an average value is taken.
+#' If, in every year, there are less then 12 pH/Total Chloride/Total Hardness then an average value is taken.
 #' Otherwise, if there is only one year with 12 or more values a simple seasonal smoother is used.
 #' If there is two years with 12 or more values then a seasonal smoother with a trend is fitted.
 #' Otherwise a model with trend and a dynamic seasonal component is fitted.
