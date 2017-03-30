87dff6f4b27ececb1ea580e6e13c385e49774dc5
Andy Teucher <andy.teucher@gov.bc.ca>
2017-03-30 11:01:59 -0700

    Get codes from ems_codes, add Alkalanity pH 4.5/5.2

--- data-raw/ems-codes.R
+++ data-raw/ems-codes.R
@@ -1,16 +1,9 @@
 library(rems)
 library(dplyr)
-library(magrittr)
 library(devtools)
 
-try(download_historic_data())
-ems_historic <- read_historic_data()
-ems_current <- get_ems_data()
-
-ems_codes <- bind_rows(ems_current, ems_historic)
-
-ems_codes %<>% select(Variable = PARAMETER, Code = PARAMETER_CODE) %>%
+ems_codes <- rems::ems_parameters %>%
+  select(Variable = PARAMETER, Code = PARAMETER_CODE) %>%
   unique() %>% arrange(Variable) %>% filter(!is.na(Variable))
 
-ems_codes %<>% as.tbl()
 use_data(ems_codes, overwrite = TRUE, compress = "xz")
