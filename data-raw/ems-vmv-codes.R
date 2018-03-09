48c897f684044e68e3a5b9ec3eb805043f4f5339
Andy Teucher <andy.teucher@gmail.com>
2018-03-09 14:25:46 -0800

    Join VMV on code and method

--- data-raw/ems-vmv-codes.R
+++ data-raw/ems-vmv-codes.R
@@ -35,9 +35,7 @@ vmv_ems_xwalk <- read_csv("data-raw/VMV_to_EMS.csv",
         filter(OTHER_SYSTEM_NAME == "EMS") %>%
         separate(OTHER_SYSTEM_CODE, c("EMS_CODE", "EMS_METHOD_CODE"), 4) %>%
         select(VMV_CODE = vmv_code,
-               # VMV_VARIABLE = variable_name,
-               VMV_UNIT = unit_code,
-               VMV_METHOD_CODE = method_code, #,
+               VMV_METHOD_CODE = method_code,
                EMS_CODE,
                EMS_METHOD_CODE,
                EMS_UNIT_CODE = OTHER_SYSTEM_UNIT_CODE
@@ -45,14 +43,30 @@ vmv_ems_xwalk <- read_csv("data-raw/VMV_to_EMS.csv",
         mutate_all(stringr::str_trim, side = "both") %>%
         distinct()
 
-# VMV_CODE should be unique but include VMV_UNIT and VMV_METHOD_CODE for
-# completeness.
+# VMV_CODE should be unique but include VMV_METHOD_CODE for completeness.
 # The combination of EMS_CODE, EMS_METHOD_CODE, and EMS_UNIT_CODE should be
 # unique.
 vmv_ems <- left_join(ems_codes, vmv_ems_xwalk,
                      by = c("EMS_CODE",
                             "EMS_METHOD_CODE",
                             "EMS_UNIT_CODE")) %>%
-        left_join(vmv_codes, by = c("VMV_CODE", "VMV_UNIT", "VMV_METHOD_CODE"))
+        left_join(vmv_codes, by = c("VMV_CODE", "VMV_METHOD_CODE")) %>%
+        select(
+                EMS_CODE,
+                EMS_VARIABLE,
+                EMS_UNIT,
+                EMS_UNIT_CODE,
+                EMS_METHOD_CODE,
+                EMS_METHOD_TITLE,
+                EMS_MDL,
+                VMV_CODE,
+                VMV_VARIABLE_CODE,
+                VMV_VARIABLE,
+                VMV_VARIABLE_TYPE,
+                VMV_UNIT,
+                VMV_UNIT_NAME,
+                VMV_METHOD_CODE,
+                VMV_METHOD_TITLE
+        )
 
 use_data(ems_codes, vmv_ems, vmv_codes, overwrite = TRUE)
