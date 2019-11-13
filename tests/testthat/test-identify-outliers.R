74dfbefba6e4d5bfd88cd65f6bae8cce7e828fc4
joethorley <joe@poissonconsulting.ca>
2019-11-13 15:40:44 -0800

    fix bug so now identifies outliers by Variable!

--- tests/testthat/test-identify-outliers.R
+++ tests/testthat/test-identify-outliers.R
@@ -25,22 +25,32 @@ test_that("adequate_unique_outliers", {
 })
 
 test_that("identify_outliers", {
-  data <- data.frame(Value2 = c(1,NA))
+  data <- data.frame(Value2 = c(1,NA), Variable = "pH")
   expect_error(identify_outliers(data), "data column names must include 'Value', 'DetectionLimit' and 'Outlier'")
   data$Value <- data$Value2
   expect_identical(identify_outliers(data, messages = FALSE)$Outlier, c(FALSE, FALSE))
   expect_message(identify_outliers(data, messages = TRUE), "Identified 0 outliers in water quality data.")
-  expect_message(identify_outliers(wqbc::dummy, sds = 2, messages = TRUE), "Identified 10 outliers in water quality data.")
-  expect_identical(identify_outliers(data.frame(Value = c(NA_real_, NA_real_)), messages = FALSE)$Outlier, c(FALSE, FALSE))
-  data <- data.frame(Value = c(rep(100, 2), rep(0, 1000)))
+  expect_message(identify_outliers(wqbc::dummy, sds = 2, messages = TRUE), "Identified 5 outliers in water quality data.")
+  expect_identical(identify_outliers(data.frame(Value = c(NA_real_, NA_real_), Variable = "pH"), messages = FALSE)$Outlier, c(FALSE, FALSE))
+  data <- data.frame(Value = c(rep(100, 2), rep(0, 1000)), Variable = "pH")
   expect_message(identify_outliers(data, messages = TRUE), "Identified 0 outliers in water quality data.")
   expect_message(identify_outliers(data, ignore_undetected = FALSE, messages = TRUE), "Identified 0 outliers in water quality data.")
-  data <- data.frame(Value = c(101, rep(100, 2), rep(0, 1000)))
+  data <- data.frame(Value = c(101, rep(100, 2), rep(0, 1000)), Variable = "pH")
   expect_message(identify_outliers(data, sds = 10, ignore_undetected = FALSE, messages = TRUE), "Identified 3 outliers in water quality data.")
-  data <- data.frame(Value = c(-101, rep(100, 2), rep(0, 1000)))
+  data <- data.frame(Value = c(-101, rep(100, 2), rep(0, 1000)), Variable = "pH")
   expect_message(identify_outliers(data, sds = 10, ignore_undetected = FALSE, messages = TRUE), "Identified 2 outliers in water quality data.")
   expect_message(identify_outliers(data, sds = 10, ignore_undetected = FALSE, large_only = FALSE, messages = TRUE), "Identified 3 outliers in water quality data.")
 
-  data <- expand.grid(Value = c(-101, rep(100, 2), rep(0, 1000)), Other = c("x", "y"))
+  data <- expand.grid(Value = c(-101, rep(100, 2), rep(0, 1000)), Other = c("x", "y"),
+                      Variable = "pH")
   expect_message(identify_outliers(data, by = "Other", sds = 10, ignore_undetected = FALSE, large_only = FALSE, messages = TRUE), "Identified 6 outliers in water quality data.")
+})
+
+test_that("identify_outliers by Variable!", {
+  data <- wqbc::dummy
+  outliers <- identify_outliers(wqbc::dummy, sds = 2, messages = FALSE)
+  expect_equivalent(outliers[names(wqbc::dummy)], wqbc::dummy[order(wqbc::dummy$Variable),])
+  expect_identical(outliers$Outlier, c(TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE,
+TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE))
 })
