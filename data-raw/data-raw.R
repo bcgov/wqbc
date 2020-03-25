aed7b38af9b991eac80bd4a3012644b384b2a892
Andy Teucher <andy.teucher@gov.bc.ca>
2020-03-25 09:32:04 -0700

    Updates to data-raw.R so full script can run (#126)

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -12,19 +12,18 @@
 
 ## The overarching script to prepare data for the package
 
-library(wqbc) # rebuild package
+devtools::load_all() # rebuild package
 source("data-raw/dummy.R")
 source("data-raw/codes.R")
 source("data-raw/limits.R")
+devtools::load_all() # rebuild package
 source("data-raw/site-limits.R")
-library(wqbc) # rebuild package
 # source("data-raw/fraser-raw.R")
 source("data-raw/fraser.R")
 # source("data-raw/map-raw.R")
 source("data-raw/map.R")
 source("data-raw/ccme.R")
 source("data-raw/data-internal.R")
-source("data-raw/ems-codes.R")
-source("data-raw/vmv-codes.R")
+source("data-raw/ems-vmv-codes.R")
 source("data-raw/stations.R")
 source("data-raw/YuePilon_hydat_dataset.R")
