e5bf26f3d34e4adaa54f5df76cd5100d85828ce4
Seb Dalgarno <seb@poissonconsulting.ca>
2020-03-24 18:01:39 -0700

    Andy's suggested changes

--- data-raw/limits-update.R
+++ data-raw/limits-update.R
@@ -1,7 +1,6 @@
 library(dplyr)
 
 load("data/codes.rda")
-load("data/limits.rda")
 
 ###### ------ create new limits table from new guidelines
 limits_new <- bcdata::bcdc_get_data(record = "85d3990a-ec0a-4436-8ebd-150de3ba0747")
@@ -34,49 +33,16 @@ limits_new <- limits_new %>%
   dplyr::ungroup()
 
 ### ensure that no duplicates
-expect_true(all(limits_new %>%
+stopifnot(all(limits_new %>%
                   dplyr::group_by(EMS_Code, Use, Term, Condition) %>%
                   dplyr::mutate(n = dplyr::n()) %>%
                   dplyr::ungroup() %>%
                   dplyr::pull(n) == 1))
 
-# limits <- limits %>%
-#   left_join(select(codes, Variable, Code), "Variable")
-#
-# limits$EMS_Code <- limits$Code
-
-# get all missing limits from old table and then remove them one by one based on discussion here:
-# https://github.com/bcgov/wqg_data/issues/83
-# tmp <- anti_join(limits, limits_new, c("EMS_Code", "Term"))
-#
-# ### remove copper total, nitrogen dissolved, nitrogen total, phosphorous dissolved limits
-# tmp <- tmp %>%
-#   filter(!(EMS_Code %in% c("EMS_P__D", "EMS_1114", "EMS_0114", "EMS_CU_T", "EMS_0104")))
-#
-# ### Term is incorrect in old table for EMS_AS_T, EMS_CLO3, EMS_MTBE
-# tmp <- tmp %>%
-#   filter(!(EMS_Code %in% c("EMS_AS_T", "EMS_MTBE", "EMS_CLO3", "EMS_CL03")))
-#
-# ### remove Chlorine Residual and Phosphorous Total because have ConditionNotes
-# tmp <- tmp %>%
-#   filter(!(EMS_Code %in% c("EMS_1016", "EMS_P__T")))
-#
-# ### remove EMS_NAPH as replace by EMS_PA14
-# tmp <- tmp %>%
-#   filter(!(EMS_Code %in% c("EMS_NAPH")))
-#
-# ### remove Alkalinity Total as replaced by Alkalinity (CaCO3) Total
-# tmp <- tmp %>%
-#   filter(!(EMS_Code == "EMS_AK_T"))
-#
-# ### confirm that all cases have been dealt with
-# expect_identical(nrow(tmp), 0L)
-
-
 ### deal with hardness equations (only include Hardness Total when both Hardness Total and Hardnes Dissolved)
 modified <- limits_new$Condition[which(stringr::str_detect(limits_new$Condition, "EMS_0107"))] %>%
   stringr::str_split_fixed("\\|", 2)
-modified <- modified[, 1]
+modified <- modified[stringr::str_detect(modified, "EMS_0107")]
 limits_new$Condition[which(stringr::str_detect(limits_new$Condition, "EMS_0107"))] <- modified
 
 ### replace Aluminum with Aluminium
@@ -85,7 +51,7 @@ limits_new$Variable %<>%
 
 ### create new codes table and grab EC_Code from old table where possible
 ec_codes <- select(codes, Code, EC_Code) %>%
-  filter(Code %in% unique(limits$EMS_Code), !is.na(EC_Code))
+  filter(Code %in% unique(limits_new$EMS_Code), !is.na(EC_Code))
 
 codes_new <- limits_new %>%
   select(Variable, Code = EMS_Code, Units) %>%
