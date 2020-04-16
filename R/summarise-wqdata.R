ffa2c27e80494c6d874113688851702713594b97
joethorley <joe@poissonconsulting.ca>
2020-04-16 15:54:15 -0700

    - Added percentiles argument to summarise_wqdata().

--- R/summarise-wqdata.R
+++ R/summarise-wqdata.R
@@ -2,14 +2,26 @@ se <- function(x, na.rm) {
   stats::sd(x, na.rm = na.rm) / sqrt(sum(!is.na(x)))
 }
 
-summarise_wqdata_by <- function(x, na.rm) {
+summarise_wqdata_by <- function(x, level, na.rm) {
   x <- x$Value
+
+  # necessary as quantiles errors if missing values and na.rm = FALSE
+  if(any(is.na(x)) && !na.rm) {
+    lowerQ <- NA_real_
+    upperQ <- NA_real_
+  } else {
+    lowerQ <- stats::quantile(x, (1-level)/2, na.rm = na.rm, names = FALSE)
+    upperQ <- stats::quantile(x, level + (1-level)/2, na.rm = na.rm, names = FALSE)
+  }
+
   tibble::tibble(
     n = if(na.rm) sum(!is.na(x)) else length(x),
     min = min(x, na.rm = na.rm),
     max = max(x, na.rm = na.rm),
     mean = mean(x, na.rm = na.rm),
     median = stats::median(x, na.rm = na.rm),
+    lowerQ = lowerQ,
+    upperQ = upperQ,
     sd = stats::sd(x, na.rm = na.rm),
     se = se(x, na.rm = na.rm))
 }
@@ -21,6 +33,8 @@ summarise_norows <- function() {
     max = double(0),
     mean = double(0),
     median = double(0),
+    lowerQ = double(0),
+    upperQ = double(0),
     sd = double(0),
     se = double(0))
 }
@@ -35,21 +49,24 @@ summarise_wqdata_norows <- function(x, by) {
 #' by Variable (character or factor) column.
 #'
 #' Provides basic summary statistics (n, min, max, mean, median, se, sd).
-#' SD; Options for quantiles, percentiles, upper/lower confidence intervals). View summary statistics in a tab of the results panel (added function to wqbc)
+#' SD; Options for quantiles, levels, upper/lower confidence intervals). View summary statistics in a tab of the results panel (added function to wqbc)
 #'
 #' @param x The data.frame to summarise.
 #' @param by A character vector of the columns in x to summarise by.
 #' @param na.rm A flag specifying whether to exclude missing values when summarising.
+#' @param level A number between 0 and 1 specifying the upper and lower percentiles.
 #' @return A tibble of the summary statistics.
 #' @export
 #' @examples
 #' data.frame(Variable = "var", Value = 1:5, stringsAsFactors = FALSE)
-summarise_wqdata <- function(x, by = NULL, na.rm = FALSE) {
+summarise_wqdata <- function(x, by = NULL, level = 0.95, na.rm = FALSE) {
   chk_data(x)
   chk_subset(colnames(x), c("Variable", "Value"))
   chk_numeric(x$Value)
   chk_character_or_factor(x$Variable)
   check_by(by, colnames(x), res_names = c("Value"))
+  chk_number(level)
+  chk_range(level)
   chk_flag(na.rm)
 
   by <- c("Variable", by)
@@ -58,7 +75,8 @@ summarise_wqdata <- function(x, by = NULL, na.rm = FALSE) {
   if(!nrow(x)) {
     x <- summarise_wqdata_norows(x, by = by)
   } else {
-    x <- plyr::ddply(x, .variables = by, .fun = summarise_wqdata_by, na.rm = na.rm)
+    x <- plyr::ddply(x, .variables = by, .fun = summarise_wqdata_by,
+                     level = level, na.rm = na.rm)
   }
   tibble::as_tibble(x)
 }
--- tests/testthat/test-summarise-wqdata.R
+++ tests/testthat/test-summarise-wqdata.R
@@ -12,6 +12,8 @@ test_that("zero rows", {
     max = double(0),
     mean = double(0),
     median = double(0),
+    lowerQ = double(0),
+    upperQ = double(0),
     sd = double(0),
     se = double(0))
 
@@ -21,21 +23,21 @@ test_that("zero rows", {
 test_that("works", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = 1, stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = "1", n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, sd = NA_real_, se = NA_real_)
+                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
   expect_identical(x, y)
 })
 
 test_that("all missing values", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = NA_real_, stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = "1", n = 1L, min = NA_real_, max = NA_real_, mean = NA_real_,
-                      median = NA_real_, sd = NA_real_, se = NA_real_)
+                      median = NA_real_, lowerQ = NA_real_, upperQ = NA_real_, sd = NA_real_, se = NA_real_)
   expect_identical(x, y)
 })
 
 test_that("some missing values", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = c(NA_real_, 1), stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = "1", n = 2L, min = NA_real_, max = NA_real_, mean = NA_real_,
-                      median = NA_real_, sd = NA_real_, se = NA_real_)
+                      median = NA_real_, lowerQ = NA_real_, upperQ = NA_real_, sd = NA_real_, se = NA_real_)
   expect_identical(x, y)
 })
 
@@ -43,14 +45,14 @@ test_that("na.rm = TRUE with some missing values", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = c(NA_real_, 1), stringsAsFactors = FALSE),
                         na.rm = TRUE)
   y <- tibble::tibble(Variable = "1", n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, sd = NA_real_, se = NA_real_)
+                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
   expect_identical(x, y)
 })
 
 test_that("multiple variables", {
   x <- summarise_wqdata(data.frame(Variable = c("1", "three"), Value = c(1, 3), stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = c("1", "three"), n = c(1L, 1L), min = c(1, 3), max = c(1, 3), mean = c(1, 3),
-                      median = c(1, 3), sd = c(NA_real_, NA_real_), se = c(NA_real_, NA_real_))
+                      median = c(1, 3), lowerQ = c(1, 3), upperQ = c(1, 3), sd = c(NA_real_, NA_real_), se = c(NA_real_, NA_real_))
 
   expect_identical(x, y)
 })
@@ -58,7 +60,7 @@ test_that("multiple variables", {
 test_that("multiple integer values", {
   x <- summarise_wqdata(data.frame(Variable = "var", Value = 1:5, stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = "var", n = 5L, min = 1L, max = 5L, mean = 3,
-                      median = 3L, sd = 1.58113883008419, se = 0.707106781186548)
+                      median = 3L, lowerQ = 1.1, upperQ = 4.9, sd = 1.58113883008419, se = 0.707106781186548)
 
   expect_equal(as.data.frame(x), as.data.frame(y))
 })
@@ -66,7 +68,7 @@ test_that("multiple integer values", {
 test_that("missing values in variables", {
   x <- summarise_wqdata(data.frame(Variable = NA_character_, Value = 1, stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = NA_character_, n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, sd = NA_real_, se = NA_real_)
+                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
 
   expect_identical(x, y)
 })
@@ -74,6 +76,6 @@ test_that("missing values in variables", {
 test_that("factor variable", {
   x <- summarise_wqdata(data.frame(Variable = factor("1"), Value = 1, stringsAsFactors = FALSE))
   y <- tibble::tibble(Variable = factor("1"), n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, sd = NA_real_, se = NA_real_)
+                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
   expect_identical(x, y)
 })
