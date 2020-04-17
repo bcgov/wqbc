6930454c33d040b8cd9807d6f0d40413cecaed22
joethorley <joe@poissonconsulting.ca>
2020-04-17 08:23:17 -0700

    switch to using NADA with or without censoring

--- R/summarise-wqdata.R
+++ R/summarise-wqdata.R
@@ -1,121 +1,173 @@
-se <- function(x, na.rm) {
-  stats::sd(x, na.rm = na.rm) / sqrt(sum(!is.na(x)))
-}
-
-summarise_wqdata_by_cenmle <- function(x, level, na.rm, dist) {
-  x$Censored <- is.na(x$Value) & !is.na(x$DetectionLimit) & x$Value <= x$DetectionLimit
-  x$Value[x$Censored] <- x$DetectionLimit[x$Censored]
-  ml <- with(x, cenmle(Value, Censored, dist = dist))
-
-  tibble::tibble(
-    n = if(na.rm) sum(!is.na(x$Value)) else nrow(x),
-    min = min(x$Value, na.rm = na.rm),
-    max = max(x$Value, na.rm = na.rm),
-    mean = mean(ml)[["mean"]],
-    median = median(ml),
-    lowerQ = unname(quantile(ml,  (1-level)/2)),
-    upperQ = unname(quantile(ml, level + (1-level)/2)),
-    sd = sd(ml),
-    se = mean(ml)[["se"]])
-}
-
-summarise_wqdata_by_stats <- function(x, level, na.rm) {
-  x <- x$Value
-
-  # necessary as quantiles errors if missing values and na.rm = FALSE
-  if(any(is.na(x)) && !na.rm) {
-    lowerQ <- NA_real_
-    upperQ <- NA_real_
-  } else {
-    lowerQ <- stats::quantile(x, (1-level)/2, na.rm = na.rm, names = FALSE)
-    upperQ <- stats::quantile(x, level + (1-level)/2, na.rm = na.rm, names = FALSE)
-  }
-
-  tibble::tibble(
-    n = if(na.rm) sum(!is.na(x)) else length(x),
-    min = min(x, na.rm = na.rm),
-    max = max(x, na.rm = na.rm),
-    mean = mean(x, na.rm = na.rm),
-    median = stats::median(x, na.rm = na.rm),
-    lowerQ = lowerQ,
-    upperQ = upperQ,
-    sd = stats::sd(x, na.rm = na.rm),
-    se = se(x, na.rm = na.rm))
-}
-
-summarise_wqdata_by <- function(x, cenmle, level, na.rm, dist) {
-  if(cenmle) return(summarise_wqdata_by_cenmle(x, level, na.rm, dist))
-  summarise_wqdata_by_stats(x, level, na.rm)
-}
-
-summarise_norows <- function(integer) {
-  value0 <- if(integer) integer(0) else double(0)
+summarise_norows <- function(x) {
+  value0 <- if(is.integer(x$Value)) integer(0) else double(0)
   tibble::tibble(
     n = integer(0),
+    ncen = integer(0),
     min = value0,
     max = value0,
     mean = double(0),
-    median = value0, # because sometimes median can be integer
+    median = double(0),
     lowerQ = double(0),
     upperQ = double(0),
     sd = double(0),
-    se = double(0))
+    se = double(0),
+    lowerCL = double(0),
+    upperCL = double(0))
+}
+
+summarise_missing_values <- function(x, censored) {
+  NA_value_ <- if(is.integer(x$Value)) NA_integer_ else NA_real_
+  ncen <- NA_integer_
+  if(!censored) {
+    ncen <- 0L
+  } else if(all(is.na(x$Value) == is.na(x$DetectionLimit))) {
+    ncen <- sum(!is.na(x$Value) & x$Value <= x$DetectionLimit)
+  }
+  tibble::tibble(
+    n = nrow(x),
+    ncen = ncen,
+    min = NA_value_,
+    max = NA_value_,
+    mean = NA_real_,
+    median = NA_real_,
+    lowerQ = NA_real_,
+    upperQ = NA_real_,
+    sd = NA_real_,
+    se = NA_real_,
+    lowerCL = NA_real_,
+    upperCL = NA_real_)
+}
+
+summarise_zero_values <- function(x, censored) {
+  ncen <- if(!censored) 0L else sum(x$Value <= x$DetectionLimit)
+  tibble::tibble(
+    n = nrow(x),
+    ncen = ncen,
+    min = 0,
+    max = max(x$Value),
+    mean = NA_real_,
+    median = NA_real_,
+    lowerQ = NA_real_,
+    upperQ = NA_real_,
+    sd = NA_real_,
+    se = NA_real_,
+    lowerCL = NA_real_,
+    upperCL = NA_real_)
+}
+
+summarise_wqdata_by <- function(x, group, censored, na.rm, conf_level, perc_range) {
+  if(na.rm) x %<>% dplyr::filter(!is.na(.data$Value))
+  if(!nrow(x)) {
+    return(summarise_norows(x))
+  }
+  if(any(is.na(x$Value))) {
+    return(summarise_missing_values(x, censored))
+  }
+
+  # get min and max before censored values altered
+  min <- min(x$Value)
+  max <- max(x$Value)
+
+  if(!censored) {
+    x$Censored <- FALSE
+  } else {
+    x$Censored <- !is.na(x$DetectionLimit) & x$Value <= x$DetectionLimit
+    x$Value[x$Censored] <- x$DetectionLimit[x$Censored]
+  }
+  if(any(x$Value == 0)) {
+    return(summarise_zero_values(x, censored))
+  }
+  ml <- with(x, cenmle(Value, Censored, dist = "lognormal", conf.int = conf_level))
+
+  est <- mean(ml)
+  quantiles <- quantile(ml,  c((1-perc_range)/2, perc_range + (1-perc_range)/2))
+
+  tibble::tibble(
+    n = nrow(x),
+    ncen = sum(x$Censored),
+    min = min,
+    max = max,
+    mean = est[["mean"]],
+    median = median(ml),
+    lowerQ = quantiles[[1]],
+    upperQ = quantiles[[2]],
+    sd = sd(ml),
+    se = est[["se"]],
+    lowerCL = est[[3]],
+    upperCL = est[[4]])
 }
 
 summarise_wqdata_norows <- function(x, by) {
-  dplyr::bind_rows(x[by], summarise_norows(is.integer(x$Value)))
+  dplyr::bind_rows(x[by], summarise_norows(x))
 }
 
 #' Summarise Water Quality Data
 #'
-#' By default provides summary statistics for water quality data in the Values (numeric) column
-#' by Variable (character or factor) column.
+#' Calculates summary statistics for water quality data
+#' using log-normal maximum-likelihood models.
 #'
-#' The user can specify additional columns to calculate the summary statistics by
-#' and/or account for non-detects using left-censored maximum-likelihood estimation.
-#' Missing values are considered to be below the detection limit (if the detection limit is defined).
+#' The data set must include a numeric 'Value' and
+#' a character or factor 'Variable' column.
+#'
+#' By default the summary statistics are independently calculated for each Variable.
+#' The user can specify additional columns to independently calculate the statistics by using the by argument.
+#'
+#' If the user wishes to calculate the statistics for groups assuming the same variance
+#' then they should specify them using the group argument.
+#' A column cannot be specified in both the by and group argument.
+#'
+#' If the user wishes to account for non-detects using left-censored maximum-likelihood
+#' (by setting censored = TRUE) the data set must also include a numeric DetectionLimit column.
+#'
+#' Missing values in the DetectionLimit column are assumed to indicate that the
+#' Values are not censored.
+#' Missing values in the Value column are always considered to be missing values.
+#' If the user wishes to exclude missing values in the Value column
+#' they should set na.rm = TRUE.
 #'
 #' @param x The data.frame to summarise.
-#' @param by A character vector of the columns in x to summarise by.
-#' @param na.rm A flag specifying whether to exclude missing values when summarising.
-#' @param cenmle A flag specifying whether to account for non-detects using left-censored maximum likelihood estimation.
-#' @param level A number between 0 and 1 specifying the upper and lower percentiles.
-#' @param dist Assumed distribution of the values for the left-censored maximum-likelihood model.
-#' @param ... Additional arguments passed to \code{\link[survival]{survreg}}.
+#' @param by A character vector specifying the columns in x to independently summarise by.
+#' @param group A character vector specifying the columns in x to summarise by assuming the same variance.
+#' @param censored A flag specifying whether to account for non-detects.
+#' @param na.rm A flag specifying whether to exclude missing Value values when summarising.
+#' @param conf_level A number between 0 and 1 specifying confidence limits.
+#' @param perc_range A number between 0 and 1 specifying the quantile range.
 #' @return A tibble of the summary statistics.
 #' @export
 #' @examples
 #' data.frame(Variable = "var", Value = 1:5, stringsAsFactors = FALSE)
-summarise_wqdata <- function(x, by = NULL, cenmle = FALSE, level = 0.95, na.rm = FALSE,
-                             dist = "lognormal") {
+summarise_wqdata <- function(x, by = NULL, group = NULL, censored = FALSE,
+                             na.rm = FALSE, conf_level = 0.95, perc_range = 0.95) {
   chk_data(x)
   check_names(x, c("Variable", "Value"))
   chk_character_or_factor(x$Variable)
   chk_numeric(x$Value)
   chk_gte(x$Value)
   check_by(by, colnames(x), res_names = c("Value", "DetectionLimit"))
-  chk_flag(cenmle)
-  chk_number(level)
-  chk_range(level)
+  check_by(group, colnames(x), res_names = c("Value", "Variable", "DetectionLimit", by))
+  chk_flag(censored)
   chk_flag(na.rm)
-  chk_string(dist)
-  chk_subset(dist, c("lognormal", "loglogistic", "weibull"))
+  chk_number(conf_level)
+  chk_range(conf_level)
+  chk_number(perc_range)
+  chk_range(perc_range)
 
-  if(cenmle) {
+  if(censored) {
     check_names(x, "DetectionLimit")
     chk_numeric(x$DetectionLimit)
     chk_gte(x$DetectionLimit)
   }
 
-  by <- c("Variable", by)
-  by <- unique(by)
+  by <- unique(c("Variable", by))
+  group <- unique(group)
 
   if(!nrow(x)) {
     x <- summarise_wqdata_norows(x, by = by)
   } else {
     x <- plyr::ddply(x, .variables = by, .fun = summarise_wqdata_by,
-                     cenmle = cenmle, level = level, na.rm = na.rm,
-                     dist = dist)
+                     group = group, censored = censored, na.rm = na.rm,
+                     conf_level = conf_level,
+                     perc_range = perc_range)
   }
   tibble::as_tibble(x)
 }
--- tests/testthat/test-summarise-wqdata.R
+++ tests/testthat/test-summarise-wqdata.R
@@ -1,7 +1,7 @@
 test_that("errors correctly", {
   chk::expect_chk_error(summarise_wqdata(data.frame(Value = 1)))
   chk::expect_chk_error(summarise_wqdata(data.frame(Variable = 1, Value = 1)))
-  chk::expect_chk_error(summarise_wqdata(data.frame(Variable = "1", Value = 1), cenmle = TRUE))
+  chk::expect_chk_error(summarise_wqdata(data.frame(Variable = "1", Value = 1), censored = TRUE))
 })
 
 test_that("zero rows", {
@@ -9,6 +9,7 @@ test_that("zero rows", {
   y <-  tibble::tibble(
     Variable = character(0),
     n = integer(0),
+    ncen = integer(0),
     min = double(0),
     max = double(0),
     mean = double(0),
@@ -16,7 +17,9 @@ test_that("zero rows", {
     lowerQ = double(0),
     upperQ = double(0),
     sd = double(0),
-    se = double(0))
+    se = double(0),
+    lowerCL = double(0),
+    upperCL = double(0))
 
   expect_identical(x,y)
 })
@@ -26,90 +29,298 @@ test_that("zero rows with integer values", {
   y <-  tibble::tibble(
     Variable = character(0),
     n = integer(0),
+    ncen = integer(0),
     min = integer(0),
     max = integer(0),
     mean = double(0),
-    median = integer(0),
+    median = double(0),
     lowerQ = double(0),
     upperQ = double(0),
     sd = double(0),
-    se = double(0))
+    se = double(0),
+    lowerCL = double(0),
+    upperCL = double(0))
 
   expect_identical(x,y)
 })
 
 test_that("works", {
-  x <- summarise_wqdata(data.frame(Variable = "1", Value = 1, stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = "1", n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
-  expect_identical(x, y)
+  set.seed(99)
+  x <- summarise_wqdata(data.frame(Variable = "1", Value = rlnorm(1000), stringsAsFactors = FALSE))
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 0L, min = 0.0455637968166263,
+    max = 36.359661036231, mean = 1.64623322183978, median = 0.976190223959382,
+    lowerQ = 0.131619030746151, upperQ = 7.24019427852939, sd = 2.23542268426457,
+    se = 0.070690272154835, lowerCL = 1.52244306862086, upperCL = 1.78008877740433)
+  expect_equal(as.data.frame(x), as.data.frame(y))
 })
 
 test_that("all missing values", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = NA_real_, stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = "1", n = 1L, min = NA_real_, max = NA_real_, mean = NA_real_,
-                      median = NA_real_, lowerQ = NA_real_, upperQ = NA_real_, sd = NA_real_, se = NA_real_)
+  y <- tibble::tibble(Variable = "1", n = 1L, ncen = 0L, min = NA_real_,
+    max = NA_real_, mean = NA_real_, median = NA_real_, lowerQ = NA_real_,
+    upperQ = NA_real_, sd = NA_real_, se = NA_real_, lowerCL = NA_real_,
+    upperCL = NA_real_)
   expect_identical(x, y)
 })
 
-test_that("some missing values", {
+test_that("all missing values censored", {
+  x <- summarise_wqdata(data.frame(Variable = "1", Value = NA_real_, DetectionLimit = 1, stringsAsFactors = FALSE), censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 1L, ncen = NA_integer_, min = NA_real_,
+    max = NA_real_, mean = NA_real_, median = NA_real_, lowerQ = NA_real_,
+    upperQ = NA_real_, sd = NA_real_, se = NA_real_, lowerCL = NA_real_,
+    upperCL = NA_real_)
+  expect_identical(x, y)
+})
+
+test_that("some missing values all censored", {
+  x <- summarise_wqdata(data.frame(Variable = "1", Value = c(1, NA_real_), DetectionLimit = 1, stringsAsFactors = FALSE), censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 2L, ncen = NA_integer_, min = NA_real_,
+    max = NA_real_, mean = NA_real_, median = NA_real_, lowerQ = NA_real_,
+    upperQ = NA_real_, sd = NA_real_, se = NA_real_, lowerCL = NA_real_,
+    upperCL = NA_real_)
+  expect_identical(x, y)
+})
+
+test_that("some missing values only non-missing censored", {
+  x <- summarise_wqdata(data.frame(Variable = "1", Value = c(1, NA_real_), DetectionLimit = c(1, NA_real_), stringsAsFactors = FALSE), censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 2L, ncen = 1L, min = NA_real_,
+    max = NA_real_, mean = NA_real_, median = NA_real_, lowerQ = NA_real_,
+    upperQ = NA_real_, sd = NA_real_, se = NA_real_, lowerCL = NA_real_,
+    upperCL = NA_real_)
+  expect_identical(x, y)
+})
+
+test_that("some missing values uncensored", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = c(NA_real_, 1), stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = "1", n = 2L, min = NA_real_, max = NA_real_, mean = NA_real_,
-                      median = NA_real_, lowerQ = NA_real_, upperQ = NA_real_, sd = NA_real_, se = NA_real_)
+  y <- tibble::tibble(Variable = "1", n = 2L, ncen = 0L, min = NA_real_,
+    max = NA_real_, mean = NA_real_, median = NA_real_, lowerQ = NA_real_,
+    upperQ = NA_real_, sd = NA_real_, se = NA_real_, lowerCL = NA_real_,
+    upperCL = NA_real_)
   expect_identical(x, y)
 })
 
-test_that("na.rm = TRUE with some missing values", {
+test_that("na.rm = TRUE with only 1 non-missing value", {
   x <- summarise_wqdata(data.frame(Variable = "1", Value = c(NA_real_, 1), stringsAsFactors = FALSE),
                         na.rm = TRUE)
-  y <- tibble::tibble(Variable = "1", n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
+  y <- tibble::tibble(Variable = "1", n = 1L, ncen = 0L, min = 1, max = 1,
+    mean = NA_real_, median = NA_real_, lowerQ = NA_real_, upperQ = NA_real_,
+    sd = NA_real_, se = NA_real_, lowerCL = NA_real_, upperCL = NA_real_)
   expect_identical(x, y)
 })
 
-test_that("multiple variables", {
+test_that("multiple variables each with 1 value", {
   x <- summarise_wqdata(data.frame(Variable = c("1", "three"), Value = c(1, 3), stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = c("1", "three"), n = c(1L, 1L), min = c(1, 3), max = c(1, 3), mean = c(1, 3),
-                      median = c(1, 3), lowerQ = c(1, 3), upperQ = c(1, 3), sd = c(NA_real_, NA_real_), se = c(NA_real_, NA_real_))
+  y <- tibble::tibble(Variable = c("1", "three"), n = c(1L, 1L), ncen = c(0L,
+0L), min = c(1, 3), max = c(1, 3), mean = c(NA_real_, NA_real_
+), median = c(NA_real_, NA_real_), lowerQ = c(NA_real_, NA_real_
+), upperQ = c(NA_real_, NA_real_), sd = c(NA_real_, NA_real_),
+    se = c(NA_real_, NA_real_), lowerCL = c(NA_real_, NA_real_
+    ), upperCL = c(NA_real_, NA_real_))
 
   expect_identical(x, y)
 })
 
-test_that("multiple integer values", {
-  x <- summarise_wqdata(data.frame(Variable = "var", Value = 1:5, stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = "var", n = 5L, min = 1L, max = 5L, mean = 3,
-                      median = 3L, lowerQ = 1.1, upperQ = 4.9, sd = 1.58113883008419, se = 0.707106781186548)
-
-  expect_equal(as.data.frame(x), as.data.frame(y))
-
+test_that("two integer values", {
   x <- summarise_wqdata(data.frame(Variable = "var", Value = 1:2, stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = "var", n = 2L, min = 1L, max = 2L, mean = 1.5,
-                      median = 1.5, lowerQ = 1.025, upperQ = 1.975, sd = 0.707106781186548, se = 0.5)
+  y <- tibble::tibble(Variable = "var", n = 2L, ncen = 0L, min = 1L,
+    max = 2L, mean = 1.5017486818551, median = 1.41421356237309,
+    lowerQ = 0.71698657342923, upperQ = 2.78945251433974, sd = 0.53649338845922,
+    se = 0.379358113041263, lowerCL = 0.920159272574001, upperCL = 2.45093340976159)
 
   expect_equal(as.data.frame(x), as.data.frame(y))
 })
 
 test_that("missing values in variables", {
-  x <- summarise_wqdata(data.frame(Variable = NA_character_, Value = 1, stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = NA_character_, n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
-
-  expect_identical(x, y)
-})
-
-test_that("factor variable", {
-  x <- summarise_wqdata(data.frame(Variable = factor("1"), Value = 1, stringsAsFactors = FALSE))
-  y <- tibble::tibble(Variable = factor("1"), n = 1L, min = 1, max = 1, mean = 1,
-                      median = 1, lowerQ = 1, upperQ = 1, sd = NA_real_, se = NA_real_)
-  expect_identical(x, y)
-})
-
-test_that("cenmle", {
-  x <- summarise_wqdata(data.frame(Variable = "var", Value = c(0.60520238844859, 1.14057345351346), DetectionLimit = 2, stringsAsFactors = FALSE), cenmle = TRUE)
-  y <- tibble::tibble(Variable = "var", n = 2L, min = 0.60520238844859,
-    max = 1.14057345351346, mean = 0.873602418302059, median = 0.830829572335628,
-    lowerQ = 0.446476627453879, upperQ = 1.54605579737476,
-    sd = 0.283906799310736, se = 0.20075242301759)
+  x <- summarise_wqdata(data.frame(Variable = NA_character_, Value = 1:2, stringsAsFactors = FALSE))
+  y <- tibble::tibble(Variable = NA_character_, n = 2L, ncen = 0L, min = 1L,
+    max = 2L, mean = 1.5017486818551, median = 1.41421356237309,
+    lowerQ = 0.71698657342923, upperQ = 2.78945251433974, sd = 0.53649338845922,
+    se = 0.379358113041263, lowerCL = 0.920159272574001, upperCL = 2.45093340976159)
 
   expect_equal(as.data.frame(x), as.data.frame(y))
 })
+
+test_that("factor variable", {
+  x <- summarise_wqdata(data.frame(Variable = factor("1"), Value = c(1,2), stringsAsFactors = FALSE))
+  y <- tibble::tibble(Variable = factor("1"), n = 2L, ncen = 0L, min = 1,
+    max = 2, mean = 1.5017486818551, median = 1.41421356237309,
+    lowerQ = 0.71698657342923, upperQ = 2.78945251433974, sd = 0.53649338845922,
+    se = 0.379358113041263, lowerCL = 0.920159272574001, upperCL = 2.45093340976159)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  x <- summarise_wqdata(data, censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 260L, min = 0.0455637968166263,
+    max = 36.359661036231, mean = 1.65082950042163, median = 0.969085874824302,
+    lowerQ = 0.128169227038409, upperQ = 7.32724581776991, sd = 2.27663879655169,
+    se = 0.0719936400660803, lowerCL = 1.52514916805004, upperCL = 1.78686655479519)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower 0s censored", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  data$Value[data$Value <= data$DetectionLimit] <- 0
+
+  x <- summarise_wqdata(data, censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 260L, min = 0,
+    max = 36.359661036231, mean = 1.65082950042163, median = 0.969085874824302,
+    lowerQ = 0.128169227038409, upperQ = 7.32724581776991, sd = 2.27663879655169,
+    se = 0.0719936400660803, lowerCL = 1.52514916805004, upperCL = 1.78686655479519)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("0s", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+  data$Value[data$Value <= data$DetectionLimit] <- 0
+
+  x <- summarise_wqdata(data)
+
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 0L, min = 0,
+    max = 36.359661036231, mean = NA_real_, median = NA_real_,
+    lowerQ = NA_real_, upperQ = NA_real_, sd = NA_real_,
+    se = NA_real_, lowerCL = NA_real_, upperCL = NA_real_)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored not quite 0", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+  data$Value[data$Value <= data$DetectionLimit] <- 0.001
+
+  x <- summarise_wqdata(data)
+
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 0L, min = 0.001,
+    max = 36.359661036231, mean = 48.8749791701325, median = 0.226523789098847,
+    lowerQ = 0.000366841259147391, upperQ = 139.878014667598,
+    sd = 10545.1967836686, se = 333.468402110748, lowerCL = 29.2671569135385,
+    upperCL = 81.6192565590778)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored conf_level", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  x <- summarise_wqdata(data, censored = TRUE, conf_level = 0.8)
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 260L, min = 0.0455637968166263,
+    max = 36.359661036231, mean = 1.65082950042163, median = 0.969085874824302,
+    lowerQ = 0.128169227038409, upperQ = 7.32724581776991, sd = 2.27663879655169,
+    se = 0.0719936400660803, lowerCL = 1.56753004900058, upperCL = 1.73855553276308)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored perc_range", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  x <- summarise_wqdata(data, censored = TRUE, perc_range = 0.5)
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 260L, min = 0.0455637968166263,
+    max = 36.359661036231, mean = 1.65082950042163, median = 0.969085874824302,
+    lowerQ = 0.483074099197225, upperQ = 1.94406496714402, sd = 2.27663879655169,
+    se = 0.0719936400660803, lowerCL = 1.52514916805004, upperCL = 1.78686655479519)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored missing value", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  data$Value[1] <- NA_real_
+
+  x <- summarise_wqdata(data, censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = NA_integer_,
+    min = NA_real_, max = NA_real_, mean = NA_real_, median = NA_real_,
+    lowerQ = NA_real_, upperQ = NA_real_, sd = NA_real_, se = NA_real_,
+    lowerCL = NA_real_, upperCL = NA_real_)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored missing value na.rm = TRUE", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  data$Value[1] <- NA_real_
+
+  x <- summarise_wqdata(data, censored = TRUE, na.rm = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 999L, ncen = 260L, min = 0.0455637968166263,
+    max = 36.359661036231, mean = 1.65145740813986, median = 0.968670543090355,
+    lowerQ = 0.127917722159096, upperQ = 7.33536061472339, sd = 2.2803164138024,
+    se = 0.0721460185668738, lowerCL = 1.52554520191114, upperCL = 1.78776188832907)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored all detection limits missing", {
+  set.seed(99)
+  data <- data.frame(Variable = "1", Value = rlnorm(1000),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  data$DetectionLimit <- NA_real_
+
+  x <- summarise_wqdata(data, censored = TRUE)
+  y <- tibble::tibble(Variable = "1", n = 1000L, ncen = 0L, min = 0.0455637968166263,
+    max = 36.359661036231, mean = 1.64623322183978, median = 0.976190223959382,
+    lowerQ = 0.131619030746151, upperQ = 7.24019427852939, sd = 2.23542268426457,
+    se = 0.070690272154835, lowerCL = 1.52244306862086, upperCL = 1.78008877740433)
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+
+test_that("lower censored by ", {
+  set.seed(99)
+  data1 <- data.frame(Variable = "1", MeanLog = 0, Value = rlnorm(1000, meanlog = 1),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+  data2 <- data.frame(Variable = "1", MeanLog = 2, Value = rlnorm(1000, meanlog = 2),
+                                   DetectionLimit = 0.5, stringsAsFactors = FALSE)
+
+  data <- rbind(data1, data2)
+
+  x <- summarise_wqdata(data, by = "MeanLog", censored = TRUE)
+
+  y <- tibble::tibble(Variable = c("1", "1"), MeanLog = c(0, 2), n = c(1000L,
+1000L), ncen = c(54L, 3L), min = c(0.123855240922235, 0.336010163606413
+), max = c(98.8358058837172, 165.557501183876), mean = c(4.48406426490208,
+12.1971429147508), median = c(2.6503014491015, 7.56195983066146
+), lowerQ = c(0.355109337091874, 1.11251993244631), upperQ = c(19.7800987961412,
+51.3997410858051), sd = c(6.11963921796074, 15.436214422828),
+    se = c(0.193519983872475, 0.488135960268779), lowerCL = c(4.14551983124041,
+    11.3308870601644), upperCL = c(4.8502559751971, 13.1296247586724
+    ))
+  expect_equal(as.data.frame(x), as.data.frame(y))
+})
+#
+# test_that("lower censored group", {
+#   set.seed(99)
+#   data1 <- data.frame(Variable = "1", MeanLog = 0, Value = rlnorm(1000, meanlog = 1),
+#                                    DetectionLimit = 0.5, stringsAsFactors = FALSE)
+#   data2 <- data.frame(Variable = "1", MeanLog = 2, Value = rlnorm(1000, meanlog = 2),
+#                                    DetectionLimit = 0.5, stringsAsFactors = FALSE)
+#
+#   data <- rbind(data1, data2)
+#
+#   x <- summarise_wqdata(data, group = "MeanLog", censored = TRUE)
+#
+#   y <- tibble::tibble(Variable = c("1", "1"), MeanLog = c(0, 2), n = c(1000L,
+# 1000L), ncen = c(54L, 3L), min = c(0.123855240922235, 0.336010163606413
+# ), max = c(98.8358058837172, 165.557501183876), mean = c(4.48406426490208,
+# 12.1971429147508), median = c(2.6503014491015, 7.56195983066146
+# ), lowerQ = c(0.355109337091874, 1.11251993244631), upperQ = c(19.7800987961412,
+# 51.3997410858051), sd = c(6.11963921796074, 15.436214422828),
+#     se = c(0.193519983872475, 0.488135960268779), lowerCL = c(4.14551983124041,
+#     11.3308870601644), upperCL = c(4.8502559751971, 13.1296247586724
+#     ))
+#   expect_equal(as.data.frame(x), as.data.frame(y))
+# })
